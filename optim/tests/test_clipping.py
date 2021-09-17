a0892ce554c3aef0a20e99456443cf93175680bc
Ying Liu <yingliufb@fb.com>
2021-09-16 17:34:36 -0700

    Add unit test for GradientClippingOptimizer
    Summary: Added unit tests for GradientClippingOptimizer
    
    Reviewed By: dstaay-fb
    
    Differential Revision: D30876265
    
    fbshipit-source-id: 762567572b712bd9dd40820f07ec21843fe014df

---
+++ optim/tests/test_clipping.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python3
+
+import unittest
+
+import torch
+from torch.autograd import Variable
+from torchrec.optim.clipping import GradientClippingOptimizer, GradientClipping
+from torchrec.optim.tests.test_utils import DummyKeyedOptimizer
+
+
+class TestGradientClippingOptimizer(unittest.TestCase):
+    def test_clip_all_gradients_norm(self) -> None:
+        # Clip all gradients to zero
+        param_1 = Variable(torch.tensor([1.0, 2.0]), requires_grad=True)
+
+        keyed_optimizer = DummyKeyedOptimizer(
+            {"param_1": param_1}, {}, [{"params": [param_1]}]
+        )
+
+        gradient_clipping_optimizer = GradientClippingOptimizer(
+            optimizer=keyed_optimizer, max_gradient=0
+        )
+
+        gradient_clipping_optimizer.zero_grad()
+        param_1.grad = torch.tensor([1.0, 2.0])
+        gradient_clipping_optimizer.step()
+
+        self.assertTrue(torch.equal(param_1.grad, torch.tensor([0.0, 0.0])))
+
+    def test_clip_no_gradients_norm(self) -> None:
+        # gradients are too small to be clipped
+        param_1 = Variable(torch.tensor([1.0, 2.0]), requires_grad=True)
+
+        keyed_optimizer = DummyKeyedOptimizer(
+            {"param_1": param_1}, {}, [{"params": [param_1]}]
+        )
+
+        gradient_clipping_optimizer = GradientClippingOptimizer(
+            optimizer=keyed_optimizer, max_gradient=1
+        )
+
+        gradient_clipping_optimizer.zero_grad()
+        param_1.grad = torch.tensor([0.5, 0.5])
+        gradient_clipping_optimizer.step()
+
+        self.assertTrue(torch.equal(param_1.grad, torch.tensor([0.5, 0.5])))
+
+    def test_clip_partial_gradients_norm(self) -> None:
+        # test partial clipping
+        param_1 = Variable(torch.tensor([1.0, 2.0]), requires_grad=True)
+
+        keyed_optimizer = DummyKeyedOptimizer(
+            {"param_1": param_1}, {}, [{"params": [param_1]}]
+        )
+
+        gradient_clipping_optimizer = GradientClippingOptimizer(
+            optimizer=keyed_optimizer, max_gradient=1
+        )
+
+        gradient_clipping_optimizer.zero_grad()
+
+        param_1.grad = torch.tensor([2.0, 4.0])
+        gradient_clipping_optimizer.step()
+
+        norm = 2.0 ** 2 + 4.0 ** 2
+        expected_grad = torch.tensor([2.0, 4.0]) * norm ** (-0.5)
+        self.assertTrue(torch.allclose(param_1.grad, expected_grad))
+
+    def test_clip_partial_gradients_norm_multi_params(self) -> None:
+        # test partial clipping
+        max_gradient = 2.0
+        param_1 = Variable(torch.tensor([1.0, 2.0]), requires_grad=True)
+        param_2 = Variable(torch.tensor([2.0, 4.0]), requires_grad=True)
+
+        keyed_optimizer = DummyKeyedOptimizer(
+            {"param_1": param_1, "param_2": param_2},
+            {},
+            [{"params": [param_1]}, {"params": [param_2]}],
+        )
+
+        gradient_clipping_optimizer = GradientClippingOptimizer(
+            optimizer=keyed_optimizer, max_gradient=max_gradient
+        )
+
+        gradient_clipping_optimizer.zero_grad()
+
+        param_1.grad = torch.tensor([2.0, 4.0])
+        param_2.grad = torch.tensor([4.0, 8.0])
+
+        gradient_clipping_optimizer.step()
+
+        print(param_1.grad, param_2.grad)
+
+        norm = (2.0 ** 2 + 4.0 ** 2 + 4.0 ** 2 + 8.0 ** 2) ** (-0.5)
+        expected_grad_1 = torch.tensor([2.0, 4.0]) * norm * max_gradient
+        expected_grad_2 = torch.tensor([4.0, 8.0]) * norm * max_gradient
+
+        print(param_1.grad, param_2.grad, expected_grad_1, expected_grad_2)
+
+        self.assertTrue(torch.allclose(param_1.grad, expected_grad_1))
+        self.assertTrue(torch.allclose(param_2.grad, expected_grad_2))
+
+    def test_clip_all_gradients_value(self) -> None:
+        # Clip all gradients to zero
+        param_1 = Variable(torch.tensor([1.0, 2.0]), requires_grad=True)
+
+        keyed_optimizer = DummyKeyedOptimizer(
+            {"param_1": param_1}, {}, [{"params": [param_1]}]
+        )
+
+        gradient_clipping_optimizer = GradientClippingOptimizer(
+            optimizer=keyed_optimizer, max_gradient=0, clipping=GradientClipping.VALUE
+        )
+
+        gradient_clipping_optimizer.zero_grad()
+        param_1.grad = torch.tensor([1.0, 2.0])
+        gradient_clipping_optimizer.step()
+
+        self.assertTrue(torch.equal(param_1.grad, torch.tensor([0.0, 0.0])))
+
+    def test_clip_no_gradients_value(self) -> None:
+        # gradients are too small to be clipped
+        param_1 = Variable(torch.tensor([1.0, 2.0]), requires_grad=True)
+
+        keyed_optimizer = DummyKeyedOptimizer(
+            {"param_1": param_1}, {}, [{"params": [param_1]}]
+        )
+
+        gradient_clipping_optimizer = GradientClippingOptimizer(
+            optimizer=keyed_optimizer, max_gradient=1.0, clipping=GradientClipping.VALUE
+        )
+
+        gradient_clipping_optimizer.zero_grad()
+        param_1.grad = torch.tensor([0.5, 0.5])
+        gradient_clipping_optimizer.step()
+
+        self.assertTrue(torch.equal(param_1.grad, torch.tensor([0.5, 0.5])))
+
+    def test_clip_gradients_value(self) -> None:
+        # test partial clipping
+        param_1 = Variable(torch.tensor([1.0, 2.0]), requires_grad=True)
+
+        keyed_optimizer = DummyKeyedOptimizer(
+            {"param_1": param_1}, {}, [{"params": [param_1]}]
+        )
+
+        gradient_clipping_optimizer = GradientClippingOptimizer(
+            optimizer=keyed_optimizer, max_gradient=1, clipping=GradientClipping.VALUE
+        )
+
+        gradient_clipping_optimizer.zero_grad()
+
+        param_1.grad = torch.tensor([2.0, 4.0])
+        gradient_clipping_optimizer.step()
+
+        expected_grad = torch.tensor([1.0, 1.0])
+
+        self.assertTrue(torch.allclose(param_1.grad, expected_grad))
+
+    def test_clip_partial_gradients_value_multi_params(self) -> None:
+        # test partial clipping
+        max_gradient = 2.0
+        param_1 = Variable(torch.tensor([1.0, 2.0]), requires_grad=True)
+        param_2 = Variable(torch.tensor([2.0, 4.0]), requires_grad=True)
+
+        keyed_optimizer = DummyKeyedOptimizer(
+            {"param_1": param_1, "param_2": param_2},
+            {},
+            [{"params": [param_1]}, {"params": [param_2]}],
+        )
+
+        gradient_clipping_optimizer = GradientClippingOptimizer(
+            optimizer=keyed_optimizer,
+            max_gradient=max_gradient,
+            clipping=GradientClipping.VALUE,
+        )
+
+        gradient_clipping_optimizer.zero_grad()
+
+        param_1.grad = torch.tensor([2.0, 4.0])
+        param_2.grad = torch.tensor([4.0, 8.0])
+
+        gradient_clipping_optimizer.step()
+
+        expected_grad_1 = torch.tensor([2.0, 2.0])
+        expected_grad_2 = torch.tensor([2.0, 2.0])
+
+        self.assertTrue(torch.allclose(param_1.grad, expected_grad_1))
+        self.assertTrue(torch.allclose(param_2.grad, expected_grad_2))
