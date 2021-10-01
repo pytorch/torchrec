01b1edcc6a9e33397307f288756145fdd7ad9f60
Hongbo Zhang (AI) <hbzhang@fb.com>
2021-09-30 18:19:01 -0700

    refactor on deepfm
    Summary:
    The diff has the following items:
    * refactor of deepfm to be non-lazy and lazy module without changing the structure
    * Make the current deepfm still tracer-friendly
    * Create torchrec/models
    * Relocate the module into torchrec/models folder
    * Refactor the user adoption on deepFM
    
    Reviewed By: wx1988
    
    Differential Revision: D31026531
    
    fbshipit-source-id: 4c04e47cf4d635727a432e36663d1b93078401e4

---
+++ modules/deepfm.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python3
+
+from typing import List
+
+import torch
+from torch import nn
+from torch.fx import wrap
+
+
+# pyre-ignore[56]: Pyre was not able to infer the type of the decorator `torch.fx.wrap`.
+@wrap
+def _get_flatten_input(inputs: List[torch.Tensor]) -> torch.Tensor:
+    return torch.cat(
+        [input.flatten(1) for input in inputs],
+        dim=1,
+    )
+
+
+class DeepFM(nn.Module):
+    r"""
+    This is the DeepFM module. Extenal source of DeepFM paper:
+    https://arxiv.org/pdf/1703.04247.pdf
+
+    This module is not to cover the end-end functionality of the published paper.
+    Instead, it is only the deep component of the publication. It is used to learn
+    high-order feature interactions. If low-order feature interaction should
+    be learnt, please use FactorizationMachine module instead, which will share
+    the same embedding input of this module.
+
+    To support modeling flexibility, we customize the key components as:
+        * Different from the public paper, we change the input from raw sparse
+          features to embeddings of the features. It can allow flexibility in
+          embedding dimensions and number of embeddings, as long as all embedding
+          tensors have the same batch size.
+        * On top of the public paper, we allow users to customize the hidden layer
+          to be any module, not limited to just MLP.
+
+    The general architecture of the module is like:
+
+                                1 x 10                  output
+                                 /|\
+                                  |                     pass into `dense_module`
+                                  |
+                                1 x 90
+                                 /|\
+                                  |                     concat
+                                  |
+                        1 x 20, 1 x 30, 1 x 40          list of embeddings
+
+    Constructor Args:
+        dense_module: nn.Module.
+            any customerized module that can be used (such as MLP) in DeepFM. The in_features
+            of this module must be equal to the elements counts. For example, the input
+            embeddings is [randn(3, 2, 3), randn(3, 4, 5)], the in_features should be:
+            2*3+4*5.
+
+    Call Args:
+        embeddings: List[torch.Tensor]:
+            The list of all embeddings (e.g. dense, common_sparse, specialized_sparse,
+            embedding_features, raw_embedding_features) in the shape of:
+                (batch_size, num_embeddings, embedding_dim)
+
+            For the ease of operation, embeddings that have the same embedding dimension
+            have the option to be stacked into a single tensor. For example, when we have
+            1 trained embedding with dimension=32, 5 native embedding with dimension=64,
+            and 3 dense features with dimension=16, we can prepare the embeddings list to
+            be the list of:
+                tensor(B, 1, 32) (trained_embedding with num_embeddings=1, embedding_dim=32)
+                tensor(B, 5, 64) (native_embedding with num_embeddings=5, embedding_dim=64)
+                tensor(B, 3, 16) (dense_features with num_embeddings=3, embedding_dim=32)
+
+            Note that: batch_size of all input tensors need to be identical.
+
+    Returns:
+        deepfm_output (torch.Tensor): output of `dense_module` with flattened and
+            concatenated `embeddings` as input.
+
+    Example:
+        >>> import torch
+        >>> from torchrec.fb.modules.deepfm import DeepFM
+        >>> from torchrec.fb.modules.mlp import LazyMLP
+        >>> batch_size = 3
+        >>> output_dim = 30
+        >>> # the input embedding are in torch.Tensor of [batch_size, num_embeddings, embedding_dim]
+        >>> input_embeddings = [
+        >>>     torch.randn(batch_size, 2, 64),
+        >>>     torch.randn(batch_size, 2, 32),
+        >>> ]
+        >>> dense_module = nn.Linear(192, output_dim)
+        >>> deepfm = DeepFM(dense_module=dense_module)
+        >>> deep_fm_output = deepfm(embeddings=input_embeddings)
+    """
+
+    def __init__(
+        self,
+        dense_module: nn.Module,
+    ) -> None:
+        super().__init__()
+        self.dense_module = dense_module
+
+    def forward(
+        self,
+        embeddings: List[torch.Tensor],
+    ) -> torch.Tensor:
+        # flatten each embedding to be [B, N, D] -> [B, N*D], then cat them all on dim=1
+        deepfm_input = _get_flatten_input(embeddings)
+        deepfm_output = self.dense_module(deepfm_input)
+        return deepfm_output
+
+
+class FactorizationMachine(nn.Module):
+    r"""
+    This is the Factorization Machine module, mentioned in the DeepFM paper:
+    https://arxiv.org/pdf/1703.04247.pdf
+
+    This module is not to cover the end-end functionality of the published paper.
+    Instead, it is only the FM part of the publication. It is used to learn
+    2nd-order feature interactions.
+
+    To support modeling flexibility, we customize the key components as:
+        * Different from the public paper, we change the input from raw sparse
+          features to embeddings of the features. It can allow flexibility in
+          embedding dimensions and number of embeddings, as long as all embedding
+          tensors have the same batch size.
+
+    The general architecture of the module is like:
+
+                                1 x 1                  output
+                                 /|\
+                                  |                     pass into `dense_module`
+                                  |
+                                1 x 90
+                                 /|\
+                                  |                     concat
+                                  |
+                        1 x 20, 1 x 30, 1 x 40          list of embeddings
+
+    Constructor Args:
+        None
+
+    Call Args:
+        embeddings: List[torch.Tensor]:
+            The list of all embeddings (e.g. dense, common_sparse, specialized_sparse,
+            embedding_features, raw_embedding_features) in the shape of:
+                (batch_size, num_embeddings, embedding_dim)
+
+            For the ease of operation, embeddings that have the same embedding dimension
+            have the option to be stacked into a single tensor. For example, when we have
+            1 trained embedding with dimension=32, 5 native embedding with dimension=64,
+            and 3 dense features with dimension=16, we can prepare the embeddings list to
+            be the list of:
+                tensor(B, 1, 32) (trained_embedding with num_embeddings=1, embedding_dim=32)
+                tensor(B, 5, 64) (native_embedding with num_embeddings=5, embedding_dim=64)
+                tensor(B, 3, 16) (dense_features with num_embeddings=3, embedding_dim=32)
+
+            Note that: batch_size of all input tensors need to be identical.
+
+    Returns:
+        output (torch.Tensor): output of fm with flattened and
+            concatenated `embeddings` as input. Expected to be [B, 1]
+
+    Example:
+        >>> batch_size = 3
+        >>> # the input embedding are in torch.Tensor of [batch_size, num_embeddings, embedding_dim]
+        >>> input_embeddings = [
+        >>>     torch.randn(batch_size, 2, 64),
+        >>>     torch.randn(batch_size, 2, 32),
+        >>> ]
+        >>> fm = FactorizationMachine()
+        >>> output = fm(embeddings=input_embeddings)
+    """
+
+    def __init__(
+        self,
+    ) -> None:
+        super().__init__()
+
+    def forward(
+        self,
+        embeddings: List[torch.Tensor],
+    ) -> torch.Tensor:
+        # flatten each embedding to be [B, N, D] -> [B, N*D], then cat them all on dim=1
+        fm_input = _get_flatten_input(embeddings)
+        sum_of_input = torch.sum(fm_input, dim=1, keepdim=True)
+        sum_of_square = torch.sum(fm_input * fm_input, dim=1, keepdim=True)
+        square_of_sum = sum_of_input * sum_of_input
+        cross_term = square_of_sum - sum_of_square
+        cross_term = torch.sum(cross_term, dim=1, keepdim=True) * 0.5  # [B, 1]
+        return cross_term
