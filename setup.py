420e5b9a91cf8da7a8f2774142a3a057a161a0db
Rahul Kindi <rahulkindi@fb.com>
2021-10-24 08:55:58 -0700

    Update setup.py to match new folder structure.
    Summary: The old setup.py was needed because the top level folder for the repo contained folders like /distributed, etc. Now, we have the top level folder have a single torchrec folder, so the setup.py needs to be changed to reflect this.
    
    Reviewed By: wx1988
    
    Differential Revision: D31886770
    
    fbshipit-source-id: 57072bbd84465167129b1d6c4c5f274afcb4b805

--- setup.py
+++ setup.py
@@ -5,12 +5,5 @@ from setuptools import setup, find_packages
 # Minimal setup configuration.
 setup(
     name="torchrec",
-    # Needed because torchrec's __init__.py is at the same level as setup.py. In other
-    # PyTorch domain libraries, setup.py exists at the same level as the top level
-    # package. TODO: Update torchrec folder organization on github prior to OSS release
-    # to be consistent with other domain libraries.
-    package_dir={"torchrec": "../torchrec"},
-    packages=find_packages(
-        "..", include=("torchrec", "torchrec.*"), exclude=("*tests",)
-    ),
+    packages=find_packages(exclude=("*tests",)),
 )
