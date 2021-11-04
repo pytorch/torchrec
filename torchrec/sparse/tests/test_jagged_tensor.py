fff4ba9cd74be7fa56d61dd9901fbf77f98aa761
Wenchen Wang <wangwenchen@fb.com>
2021-11-04 14:23:02 -0700

    Add feature process logic in AdsGeneralPooledEmbeddingArch
    Summary: As titled
    
    Reviewed By: JadeNie
    
    Differential Revision: D31568073
    
    fbshipit-source-id: f31788013c20deae54dab7e7582c39d95ec0ae5c

--- torchrec/sparse/tests/test_jagged_tensor.py
+++ torchrec/sparse/tests/test_jagged_tensor.py
@@ -13,40 +13,7 @@ from torchrec.sparse.jagged_tensor import (
     KeyedTensor,
     KeyedJaggedTensor,
 )
-
-
-def keyed_jagged_tensor_equals(
-    kjt1: Optional[KeyedJaggedTensor], kjt2: Optional[KeyedJaggedTensor]
-) -> bool:
-    def _tensor_eq_or_none(
-        t1: Optional[torch.Tensor], t2: Optional[torch.Tensor]
-    ) -> bool:
-        if t1 is None and t2 is None:
-            return True
-        elif t1 is None and t2 is not None:
-            return False
-        elif t1 is not None and t2 is None:
-            return False
-        else:
-            assert t1 is not None
-            assert t2 is not None
-            return torch.equal(t1, t2) and t1.dtype == t2.dtype
-
-    if kjt1 is None and kjt2 is None:
-        return True
-    elif kjt1 is None and kjt2 is not None:
-        return False
-    elif kjt1 is not None and kjt2 is None:
-        return False
-    else:
-        assert kjt1 is not None
-        assert kjt2 is not None
-        return (
-            kjt1.keys() == kjt2.keys()
-            and _tensor_eq_or_none(kjt1.lengths(), kjt2.lengths())
-            and _tensor_eq_or_none(kjt1.values(), kjt2.values())
-            and _tensor_eq_or_none(kjt1.weights(), kjt2.weights())
-        )
+from torchrec.sparse.tests.tests_utils import keyed_jagged_tensor_equals
 
 
 def _compute_translated_lengths(
