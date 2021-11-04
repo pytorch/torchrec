a237d8756caa3768d902f07a3fe08d87b4760409
Colin Taylor <colin2328@fb.com>
2021-11-04 10:18:39 -0700

    upstream random dataset from pytorch recipes to torchrec
    Summary: Adds random as a dataset to torchrec/datasets (upstreamed from recipes)
    
    Reviewed By: rkindi
    
    Differential Revision: D32157218
    
    fbshipit-source-id: 8fb9f639a945d01be703531c4ec4fb025320e599

--- torchrec/datasets/__init__.py
+++ torchrec/datasets/__init__.py
@@ -2,4 +2,5 @@
 
 import torchrec.datasets.criteo  # noqa
 import torchrec.datasets.movielens  # noqa
+import torchrec.datasets.random  # noqa
 import torchrec.datasets.utils  # noqa
--- torchrec/datasets/utils.py
+++ torchrec/datasets/utils.py
@@ -3,6 +3,7 @@
 import csv
 import math
 import random
+from dataclasses import dataclass
 from functools import partial
 from io import IOBase
 from typing import (
@@ -16,10 +17,42 @@ from typing import (
     TypeVar,
 )
 
+import torch
 from iopath.common.file_io import PathManager, PathManagerFactory
 from torch.utils.data import IterDataPipe, functional_datapipe, get_worker_info
+from torchrec.sparse.jagged_tensor import KeyedJaggedTensor
 
 PATH_MANAGER_KEY = "torchrec"
+
+
+@dataclass
+class Batch:
+    dense_features: torch.Tensor
+    sparse_features: KeyedJaggedTensor
+    labels: torch.Tensor
+
+    def to(self, device: torch.device, non_blocking: bool = False) -> "Batch":
+        return Batch(
+            dense_features=self.dense_features.to(
+                device=device, non_blocking=non_blocking
+            ),
+            sparse_features=self.sparse_features.to(
+                device=device, non_blocking=non_blocking
+            ),
+            labels=self.labels.to(device=device, non_blocking=non_blocking),
+        )
+
+    def record_stream(self, stream: torch.cuda.streams.Stream) -> None:
+        self.dense_features.record_stream(stream)
+        self.sparse_features.record_stream(stream)
+        self.labels.record_stream(stream)
+
+    def pin_memory(self) -> "Batch":
+        return Batch(
+            dense_features=self.dense_features.pin_memory(),
+            sparse_features=self.sparse_features.pin_memory(),
+            labels=self.labels.pin_memory(),
+        )
 
 
 class _IdxFilter(IterDataPipe):
