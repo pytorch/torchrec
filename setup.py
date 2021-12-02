e938e0b769e2d89d0469ba0c60309ec003d144cb
Anirudh Sudarshan <anirudhs@fb.com>
2021-12-02 09:38:03 -0800

    Extend TorchRec setup.py to install fbgemm
    Reviewed By: colin2328
    
    Differential Revision: D32421928
    
    fbshipit-source-id: 62b6c10a0a8943b41d3de9a528c83b8cb1b48399

--- setup.py
+++ setup.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 
+import os
+import sys
+
 from setuptools import setup, find_packages
+
+if "--skip_fbgemm" in sys.argv:
+    skip_fbgemm_installation = True
+    sys.argv.remove("--skip_fbgemm")
+
+if not skip_fbgemm_installation:
+    print("Installing fbgemm_gpu")
+    torchrec_dir = os.getcwd()
+    os.chdir("third_party/fbgemm/fbgemm_gpu/")
+    os.system("python setup.py build develop")
+    os.chdir(torchrec_dir)
+else:
+    print("Skipping fbgemm_gpu installation")
 
 # Minimal setup configuration.
 setup(
