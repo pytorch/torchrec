b488c4999d0f80203354c050c61c4fae024749ab
Colin Taylor <colin2328@fb.com>
2022-02-16 13:11:55 -0800

    manually import fsdp from torch.distributed on OSS
    Differential Revision: D34282809
    
    fbshipit-source-id: 73b93c74001196d2d75a46dff6c908472c878ede

--- torchrec/distributed/train_pipeline.py
+++ torchrec/distributed/train_pipeline.py
@@ -23,6 +23,7 @@ from typing import (
 
 import torch
 from torch.autograd.profiler import record_function
+from torch.distributed import fsdp  # noqa
 from torch.distributed.fsdp.fully_sharded_data_parallel import FullyShardedDataParallel
 from torch.fx.node import Node
 from torch.nn.parallel import DistributedDataParallel
