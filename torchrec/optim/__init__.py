5bb1e50d7c44b6ced2c88ec94b510fcc489afc29
Colin Taylor <colin2328@fb.com>
2022-02-22 22:58:03 -0800

    add documentation for optimizer submodule
    Summary: as title
    
    Reviewed By: divchenko
    
    Differential Revision: D34187863
    
    fbshipit-source-id: de30d4a7469e1f73adf60b564635029877c5c0a9

--- torchrec/optim/__init__.py
+++ torchrec/optim/__init__.py
@@ -5,14 +5,22 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+"""Torchrec Optimizers
+
+Torchrec contains a special optimizer called KeyedOptimizer. KeyedOptimizer exposes the state_dict with meaningful keys- it enables  loading both
+torch.tensor and `ShardedTensor <https://github.com/pytorch/pytorch/issues/55207>`_ in place, and it prohibits loading an empty state into already initialized KeyedOptimizer and vise versa.
+
+It also contains several modules wrapping KeyedOptimizer, called CombinedOptimizer and OptimizerWrapper
+"""
+
 from torchrec.optim.clipping import GradientClipping, GradientClippingOptimizer  # noqa
 from torchrec.optim.fused import FusedOptimizer, FusedOptimizerModule  # noqa
-from torchrec.optim.keyed import (
+from torchrec.optim.keyed import (  # noqa
     KeyedOptimizer,
     CombinedOptimizer,
     KeyedOptimizerWrapper,
     OptimizerWrapper,
-)  # noqa
+)
 from torchrec.optim.warmup import WarmupPolicy, WarmupStage, WarmupOptimizer  # noqa
 
 from . import clipping  # noqa
