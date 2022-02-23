6cf7482a400cbd3e38fbd4f2e13df7ea874b8e00
Zheng Yan <zyan@fb.com>
2022-02-22 22:00:30 -0800

    add overview to __init__ in inference
    Differential Revision: D34411386
    
    fbshipit-source-id: a38a1b06cf830a883d24f6fc162c90e97b642c42

--- torchrec/inference/__init__.py
+++ torchrec/inference/__init__.py
@@ -5,5 +5,22 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+"""Torchrec Inference
+
+Torchrec inference provides a Torch.Deploy based library for GPU inference.
+
+These includes:
+    - Model packaging in Python
+        - `PredictModule` and `PredictFactory` are the contracts between the Python model authoring and the C++ model serving.
+        - `PredictFactoryPackager` can be used to package a PredictFactory class using torch.package.
+    - Model serving in C++
+        - `BatchingQueue` is a generalized config-based request tensor batching implementation.
+        - `GPUExecutor` handles the forward call into the inference model inside Torch.Deploy.
+
+We implemented an example of how to use this library with the TorchRec DLRM model.
+    - `examples/dlrm/dlrm_packager.py`: this demonstrates how to export the DLRM model as a torch.package.
+    - `examples/dlrm/modules/dlrm_predict.py`: this shows how to use `PredictModule` and `PredictFactory` based on an existing model.
+"""
+
 from . import model_packager  # noqa
 from . import modules  # noqa
