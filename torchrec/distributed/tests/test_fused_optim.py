b8fea4d23057c9c02921ff1a05fb07a94fa71dac
Xing Liu <xingl@fb.com>
2022-03-02 10:54:32 -0800

    Create empty optim when lenght of fused_optim is 0 (#122)
    Summary:
    Pull Request resolved: https://github.com/pytorch/torchrec/pull/122
    
    As title
    
    Reviewed By: colin2328
    
    Differential Revision: D34361822
    
    fbshipit-source-id: 3300dca9d436fcce4468529e89bda5cc4a23c998

--- torchrec/distributed/tests/test_fused_optim.py
+++ torchrec/distributed/tests/test_fused_optim.py
@@ -277,7 +277,6 @@ class ModelParallelTest(ModelParallelTestBase):
 
         # Load model & optimizer states from the global model.
         _copy_state_dict(local_model.state_dict(), global_model.state_dict())
-        # pyre-ignore [16]
         for param_name, local_state in local_model.fused_optimizer.state_dict()[
             "state"
         ].items():
