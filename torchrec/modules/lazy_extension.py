42959cd998b22d1d723184a68d63c1a22c515039
Pyre Bot Jr <>
2022-03-15 18:37:31 -0700

    suppress errors in `fbcode/torchrec` - batch 1
    Differential Revision: D34914097
    
    fbshipit-source-id: d163d59e72e6db983e9a49862f7a6a08846d316d

--- torchrec/modules/lazy_extension.py
+++ torchrec/modules/lazy_extension.py
@@ -34,6 +34,10 @@ def _apply_functions_after_first_forward(
 ) -> None:
     _functions_to_lazy_apply = getattr(module, "_functions_to_lazy_apply", None)
     if _functions_to_lazy_apply is not None:
+        # pyre-fixme[29]:
+        #  `Union[BoundMethod[typing.Callable(torch.Tensor.__iter__)[[Named(self,
+        #  torch.Tensor)], typing.Iterator[typing.Any]], torch.Tensor], torch.Tensor,
+        #  torch.nn.modules.module.Module]` is not a function.
         for fn in _functions_to_lazy_apply:
             module.apply(fn)
         delattr(module, "_functions_to_lazy_apply")
