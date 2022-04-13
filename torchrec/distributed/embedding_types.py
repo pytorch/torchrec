8106bfac2697db7eb899d6e4f11fa07e4cd22afe
Joshua Deng <joshuadeng@fb.com>
2022-04-13 09:42:47 -0700

    Make RW sharding CUDA dependent
    Summary:
    GLOO doesn't support the reduce scatter op.
    
    For sequence embeddings, however, RW sharding doesn't use reduce scatter, only AlltoAll, so it is supported for GLOO backend. EmbeddingCollectionSharder overrides the sharding types and includes RW for all cases
    
    Reviewed By: dstaay-fb
    
    Differential Revision: D35598352
    
    fbshipit-source-id: 61bf971f6042df9b7e21f099a479306bb61d38e2

--- torchrec/distributed/embedding_types.py
+++ torchrec/distributed/embedding_types.py
@@ -246,12 +246,14 @@ class BaseEmbeddingSharder(ModuleSharder[M]):
         types = [
             ShardingType.DATA_PARALLEL.value,
             ShardingType.TABLE_WISE.value,
-            ShardingType.ROW_WISE.value,
             ShardingType.COLUMN_WISE.value,
             ShardingType.TABLE_COLUMN_WISE.value,
         ]
         if compute_device_type in {"cuda"}:
-            types.append(ShardingType.TABLE_ROW_WISE.value)
+            types += [
+                ShardingType.ROW_WISE.value,
+                ShardingType.TABLE_ROW_WISE.value,
+            ]
 
         return types
 
