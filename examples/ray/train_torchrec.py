0aa1417345c6567967d469705ba91a1cad3a7a5e
Colin Taylor <colin2328@fb.com>
2022-05-26 14:13:19 -0700

    Back out "add DLRMTrain module to torchrec/models/dlrm.py as it is resused in multiple examples" (#377)
    Summary:
    Pull Request resolved: https://github.com/pytorch/torchrec/pull/377
    
    Original commit changeset: bce2a4a8c625
    
    Original Phabricator Diff: D36617207 (https://github.com/pytorch/torchrec/commit/99ccaa414a57279f0d99d1324618db746759e35e)
    
    Reviewed By: rkindi
    
    Differential Revision: D36709447
    
    fbshipit-source-id: 2e53af0171093517ac561838f57387d4601f3dda

--- examples/ray/train_torchrec.py
+++ examples/ray/train_torchrec.py
@@ -11,6 +11,7 @@ import os
 from typing import cast, List, Optional
 
 import torch
+from dlrm_train import DLRMTrain
 from fbgemm_gpu.split_embedding_configs import EmbOptimType as OptimType
 from torch import distributed as dist, nn
 from torch.utils.data import DataLoader
@@ -20,7 +21,6 @@ from torchrec.distributed import TrainPipelineSparseDist
 from torchrec.distributed.embeddingbag import EmbeddingBagCollectionSharder
 from torchrec.distributed.model_parallel import DistributedModelParallel
 from torchrec.distributed.types import ModuleSharder
-from torchrec.models.dlrm import DLRMTrain
 from torchrec.modules.embedding_configs import EmbeddingBagConfig
 from torchrec.modules.embedding_modules import EmbeddingBagCollection
 from torchrec.optim.keyed import KeyedOptimizerWrapper
