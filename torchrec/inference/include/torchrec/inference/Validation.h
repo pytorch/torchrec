b4013858678705605532bcc225a066d42cc7bcb7
Shabab Ayub <shababayub@fb.com>
2022-06-30 08:48:26 -0700

    Validate features of input request (#482)
    Summary:
    Pull Request resolved: https://github.com/pytorch/torchrec/pull/482
    
    Why is validation in `populateTrecRequestFromTensorMap` in `TrecGpuUtils.cpp`?
    - because I didn't want to iterate through all the features twice. we already have to iterate through all the features once to convert the request to a torchrec request, to be efficient, the validation should happen during this conversion as well
    - if `populateTrecRequestFromTensorMap` throws an exception, it is handled by `TrecGpuMon.cpp` already
    
    Reviewed By: zyan0
    
    Differential Revision: D37385890
    
    fbshipit-source-id: 7f6d2271ba261788cddd1a4d88b89dda757e6a5f

---
+++ torchrec/inference/include/torchrec/inference/Validation.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include "torchrec/inference/Types.h"
+
+namespace torchrec {
+
+// Returns whether sparse features (KeyedJaggedTensor) are valid.
+// Currently validates:
+//  1. Whether sum(lengths) == size(values)
+//  2. Whether there are negative values in lengths
+bool validateSparseFeatures(at::Tensor& values, at::Tensor& lengths);
+
+// Returns whether dense features are valid.
+// Currently validates:
+//  1. Whether the size of values is divisable by batch size (request level)
+bool validateDenseFeatures(at::Tensor& values, size_t batchSize);
+
+} // namespace torchrec
