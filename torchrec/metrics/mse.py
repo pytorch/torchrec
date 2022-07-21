1eac3a418e95f411db24109b94aca953b270d7fb
Chien-Chin Huang <chienchin@fb.com>
2022-07-21 12:25:28 -0700

    Avoid MSE divided by zero issue (#534)
    Summary:
    Pull Request resolved: https://github.com/pytorch/torchrec/pull/534
    
    The original implementation does not check if denominator is zero. This PR fixes the issue.
    
    Reviewed By: yachyv7
    
    Differential Revision: D38038771
    
    fbshipit-source-id: 23bb6993cda272faeb90322b5b80eae062e93a23

--- torchrec/metrics/mse.py
+++ torchrec/metrics/mse.py
@@ -24,13 +24,17 @@ WEIGHTED_NUM_SAMPES = "weighted_num_samples"
 def compute_mse(
     error_sum: torch.Tensor, weighted_num_samples: torch.Tensor
 ) -> torch.Tensor:
-    return error_sum / weighted_num_samples
+    return torch.where(
+        weighted_num_samples == 0.0, 0.0, error_sum / weighted_num_samples
+    ).double()
 
 
 def compute_rmse(
     error_sum: torch.Tensor, weighted_num_samples: torch.Tensor
 ) -> torch.Tensor:
-    return torch.sqrt(error_sum / weighted_num_samples)
+    return torch.where(
+        weighted_num_samples == 0.0, 0.0, torch.sqrt(error_sum / weighted_num_samples)
+    ).double()
 
 
 def compute_error_sum(
