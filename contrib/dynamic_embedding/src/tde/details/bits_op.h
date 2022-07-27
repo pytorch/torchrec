c30c2f46c371bc4678be0ffc2542582248a29b55
Zilin Zhu <zilinzhu@tencent.com>
2022-07-27 11:33:13 +0800

    Add generic ctz (#13)

--- contrib/dynamic_embedding/src/tde/details/bits_op.h
+++ contrib/dynamic_embedding/src/tde/details/bits_op.h
@@ -9,16 +9,31 @@ template <typename T>
 struct Clz {
   int operator()(T v) const;
 };
+
+template <typename T>
+struct Ctz {
+  int operator()(T v) const;
+};
 } // namespace bits_impl
 
 /**
- * Returns the number of leading 0-bits in x, starting at the most significant
- * bit position. If x is 0, the result is undefined.
+ * Returns the number of leading 0-bits in t, starting at the most significant
+ * bit position. If t is 0, the result is undefined.
  */
 template <typename T>
 inline int Clz(T t) {
   bits_impl::Clz<T> clz;
   return clz(t);
+}
+
+/**
+ * Returns the number of trailing 0-bits in t, starting at the least significant
+ * bit position. If t is 0, the result is undefined.
+ */
+template <typename T>
+inline int Ctz(T t) {
+  bits_impl::Ctz<T> ctz;
+  return ctz(t);
 }
 
 } // namespace tde::details
