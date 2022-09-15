2fcc653bf0a611b7df5456891e3c6c8bc5abfee6
Zilin Zhu <zilinzhu@tencent.com>
2022-09-15 07:18:04 +0000

    [tde] Migrate bits op and add infra for c++ code

---
+++ torchrec/csrc/dynamic_embedding/details/bits_op.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <cstdint>
+#include <type_traits>
+
+namespace torchrec::dynamic_embedding::details {
+
+namespace bits_impl {
+template <typename T>
+struct Clz {
+  int operator()(T v) const;
+};
+
+template <typename T>
+struct Ctz {
+  int operator()(T v) const;
+};
+} // namespace bits_impl
+
+/**
+ * Returns the number of leading 0-bits in t, starting at the most significant
+ * bit position. If t is 0, the result is undefined.
+ */
+template <typename T>
+inline int clz(T t) {
+  bits_impl::Clz<T> clz;
+  return clz(t);
+}
+
+/**
+ * Returns the number of trailing 0-bits in t, starting at the least significant
+ * bit position. If t is 0, the result is undefined.
+ */
+template <typename T>
+inline int ctz(T t) {
+  bits_impl::Ctz<T> ctz;
+  return ctz(t);
+}
+
+} // namespace torchrec::dynamic_embedding::details
