863862484a8eb90d344b00f6c5d1a53202608f20
Zilin Zhu <zilinzhu@tencent.com>
2022-09-22 14:21:06 -0700

    Migrate bits op and add infra for c++ code (#633)
    Summary:
    In this PR, we are migrating the `ctz` and `clz` to the top level of torchrec. They are generic versions of `__builtin_clz` and `__builtin_clz` that work on different sizes of int and compilers. We also added `CMakeLists.txt` and change the folder structure for further C++ code.
    
    To compile and test the current code, one could:
    
    ```bash
    mkdir build && cd build
    cmake -DBUILD_TEST=ON ..
    make
    ctest -v
    ```
    
    Thank you for your time on this PR.
    
    cc reyoung colin2328
    
    Pull Request resolved: https://github.com/pytorch/torchrec/pull/633
    
    Reviewed By: zyan0
    
    Differential Revision: D39738418
    
    Pulled By: colin2328
    
    fbshipit-source-id: 286fad2d933cb2a5854507938181d8b89823de01

---
+++ torchrec/csrc/dynamic_embedding/details/ctz_impl.cpp
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <torchrec/csrc/dynamic_embedding/details/bits_op.h>
+
+namespace torchrec::bits_impl {
+
+template <typename T>
+struct CtzImpl {
+  /**
+   * Naive implementation for no __builtin_ctz
+   * @param v
+   * @return
+   */
+  int operator()(T v) const {
+    if (v == 0)
+      return -1;
+    int result = 0;
+    while ((v & 1) == 0) {
+      v >>= 1;
+      result += 1;
+    }
+    return result;
+  }
+};
+
+template <>
+struct CtzImpl<unsigned int> {
+  int operator()(unsigned int v) const {
+    return __builtin_ctz(v);
+  }
+};
+
+template <>
+struct CtzImpl<int> {
+  int operator()(int v) const {
+    return __builtin_ctz(static_cast<unsigned int>(v));
+  }
+};
+
+template <>
+struct CtzImpl<unsigned long> {
+  int operator()(unsigned long v) const {
+    return __builtin_ctzl(v);
+  }
+};
+
+template <>
+struct CtzImpl<long> {
+  int operator()(long v) const {
+    return __builtin_ctzl(static_cast<unsigned long>(v));
+  }
+};
+
+template <>
+struct CtzImpl<unsigned long long> {
+  int operator()(unsigned long long v) const {
+    return __builtin_ctzll(v);
+  }
+};
+
+template <>
+struct CtzImpl<long long> {
+  int operator()(long long v) const {
+    return __builtin_ctzll(static_cast<unsigned long long>(v));
+  }
+};
+
+template <typename T>
+int Ctz<T>::operator()(T v) const {
+  CtzImpl<T> ctz;
+  return ctz(v);
+}
+
+template struct Ctz<int>;
+template struct Ctz<unsigned int>;
+template struct Ctz<long>;
+template struct Ctz<unsigned long>;
+template struct Ctz<long long>;
+template struct Ctz<unsigned long long>;
+
+// only for unittests
+template struct Ctz<int8_t>;
+template struct Ctz<uint8_t>;
+} // namespace torchrec::bits_impl
