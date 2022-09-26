f45071532bdbc17532f0546633447deb8b9b4011
Zilin Zhu <zilinzhu@tencent.com>
2022-09-26 06:20:28 +0000

    add comment for Bitmap

--- torchrec/csrc/dynamic_embedding/details/bitmap.h
+++ torchrec/csrc/dynamic_embedding/details/bitmap.h
@@ -11,14 +11,32 @@
 
 namespace torchrec {
 
+/**
+ * Bitmap
+ * 
+ * A bitmap for recording whether num_bits of slots are
+ *  occupied or free. 
+ */
 template <typename T = uint32_t>
 struct Bitmap {
   explicit Bitmap(int64_t num_bits);
   Bitmap(const Bitmap&) = delete;
   Bitmap(Bitmap&&) noexcept = default;
 
+  /**
+   * Returns the position of the next free slot.
+   * If the bitmap is full, return `num_total_bits_`.
+   */
   int64_t next_free_bit();
+
+  /**
+   * Set the slot of position `offset` to free.
+   */
   void free_bit(int64_t offset);
+
+  /**
+   * Returns if all slots in the bitmap is occupied.
+   */
   bool full() const;
 
   static constexpr int64_t num_bits_per_value = sizeof(T) * 8;
