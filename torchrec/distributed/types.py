dfdc5a741a5db2635e948c55d9413adce51d8dc3
Leon Gao <leongao@meta.com>
2022-11-09 01:11:47 -0800

    fix copyMixIn (#801)
    Summary:
    Pull Request resolved: https://github.com/pytorch/torchrec/pull/801
    
    * fix copyMixIn in trec modules
    * fix unittest
    * refactor to reuse utils code and correct overridence behavior
    
    Reviewed By: s4ayub
    
    Differential Revision: D41143674
    
    fbshipit-source-id: d95e8d70346a330c1bd8808501b2d92081ea177f

--- torchrec/distributed/types.py
+++ torchrec/distributed/types.py
@@ -12,6 +12,7 @@ from enum import Enum, unique
 from typing import Any, Callable, Dict, Generic, Iterator, List, Optional, Type, TypeVar
 
 from torch.autograd.profiler import record_function
+from torchrec.types import ModuleNoCopyMixin
 
 try:
     # For python 3.6 and below, GenericMeta will be used by
@@ -498,16 +499,6 @@ class ShardingEnv:
         return cls(world_size, rank, None)
 
 
-class ModuleCopyMixin:
-    """
-    A mixin to allow modules to override copy behaviors in DMP.
-    """
-
-    def copy(self, device: torch.device) -> nn.Module:
-        # pyre-ignore [16]
-        return self.to(device)
-
-
 class FeatureShardingMixIn:
     """
     Feature Sharding Interface to provide sharding-aware feature metadata.
@@ -552,7 +543,7 @@ class ShardedModule(
     abc.ABC,
     nn.Module,
     Generic[CompIn, DistOut, Out, ShrdCtx],
-    ModuleCopyMixin,
+    ModuleNoCopyMixin,
     ModuleShardingMixIn,
 ):
     """
