f3753038875c8da459da137f817fcb474316f87c
Charlie Yan <charlieyan@meta.com>
2022-11-09 06:10:45 -0800

    Delete stub file to enable mypy check (#88701)
    Summary:
    X-link: https://github.com/pytorch/pytorch/pull/88701
    
    X-link: https://github.com/pytorch/tnt/pull/269
    
    X-link: https://github.com/facebookresearch/ReAgent/pull/691
    
    X-link: https://github.com/pytorch/torchsnapshot/pull/129
    
    Pull Request resolved: https://github.com/pytorch/torchrec/pull/799
    
    X-link: https://github.com/facebookresearch/detectron2/pull/4649
    
    Context in https://fburl.com/4irjskbe
    
    This change deletes distributed.pyi, so that lintrunner will run mypy on distributed.py for typing check.
    It also helps to fix a lot pyre false alarms, so that we can remove those pyre suppressions.
    
    Differential Revision: D41028360
    
    fbshipit-source-id: 6c010352a184790f58cd9730b35e68043de3ca2d

--- torchrec/distributed/fused_embedding.py
+++ torchrec/distributed/fused_embedding.py
@@ -65,9 +65,6 @@ class ShardedFusedEmbeddingCollection(
             if isinstance(sharding, DpPooledEmbeddingSharding):
                 self._lookups[index] = DistributedDataParallel(
                     module=lookup,
-                    # pyre-fixme[6]: For 2nd param expected
-                    #  `Optional[Sequence[Union[int, device]]]` but got
-                    #  `List[Optional[device]]`.
                     device_ids=[device],
                     process_group=env.process_group,
                     gradient_as_bucket_view=True,
