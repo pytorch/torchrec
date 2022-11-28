98cc6e1c906432983ac18f9b38e16eb1fd078073
Leon Gao <leongao@meta.com>
2022-11-28 14:50:26 -0800

    torchscript inference model
    Differential Revision: D41513022
    
    fbshipit-source-id: c45b50a87fac83f5a0a833431bef2e861073d3af

--- torchrec/quant/utils.py
+++ torchrec/quant/utils.py
@@ -6,27 +6,58 @@
 # LICENSE file in the root directory of this source tree.
 
 
+from typing import Union
+
 import torch
 from torch import nn
 from torchrec.distributed.model_parallel import DistributedModelParallel
+from torchrec.distributed.quant_embeddingbag import ShardedQuantEmbeddingBagCollection
 from torchrec.quant.embedding_modules import (
     EmbeddingBagCollection as QuantEmbeddingBagCollection,
 )
 
 
-def populate_fx_names(quant_ebc: QuantEmbeddingBagCollection) -> None:
+def populate_fx_names(
+    quant_ebc: Union[QuantEmbeddingBagCollection, ShardedQuantEmbeddingBagCollection]
+) -> None:
     """
     Assigns fx path to non registered lookup modules. This allows the Torchrec tracer to fallback to
     emb_module._fx_path for table batched embeddings.
     """
-    for emb_configs, emb_module in zip(
-        quant_ebc._key_to_tables, quant_ebc._emb_modules
+    if isinstance(quant_ebc, QuantEmbeddingBagCollection):
+        for emb_configs, emb_module in zip(
+            quant_ebc._key_to_tables, quant_ebc._emb_modules
+        ):
+            table_names = []
+            for config in emb_configs:
+                table_names.append(config.name)
+            joined_table_names = ",".join(table_names)
+            emb_module._fx_path = f"emb_module.{joined_table_names}"
+    elif isinstance(quant_ebc, ShardedQuantEmbeddingBagCollection):
+        for i, (emb_module, emb_dist_module) in enumerate(
+            zip(quant_ebc._lookups, quant_ebc._output_dists)
+        ):
+            embedding_fx_path = f"embedding_lookup.sharding_{i}"
+            emb_module._fx_path = embedding_fx_path
+            emb_dist_module._fx_path = f"embedding_dist.{i}"
+            for rank, rank_module in enumerate(emb_module._embedding_lookups_per_rank):
+                rank_fx_path = f"{embedding_fx_path}.rank_{rank}"
+                rank_module._fx_path = rank_fx_path
+                for group, group_module in enumerate(rank_module._emb_modules):
+                    group_module._fx_path = f"{rank_fx_path}.group_{group}"
+                    group_module._emb_module._fx_path = (
+                        f"{rank_fx_path}.group_{group}.tbe"
+                    )
+
+
+def recursive_populate_fx_names(module: nn.Module) -> None:
+    if isinstance(module, QuantEmbeddingBagCollection) or isinstance(
+        module, ShardedQuantEmbeddingBagCollection
     ):
-        table_names = []
-        for config in emb_configs:
-            table_names.append(config.name)
-        joined_table_names = ",".join(table_names)
-        emb_module._fx_path = f"emb_module.{joined_table_names}"
+        populate_fx_names(module)
+        return
+    for submodule in module.children():
+        recursive_populate_fx_names(submodule)
 
 
 def meta_to_cpu_placement(module: DistributedModelParallel) -> None:
