13a765358f5da37ee49983db54d1113eddb902d4
ylgh <ylgh@ip-172-31-16-248.us-east-2.compute.internal>
2022-12-09 12:54:26 -0800

    Exclude some files from top level directory (#872)
    Summary:
    This should fix
    
    https://github.com/pytorch/torchrec/issues/853
    
    Pull Request resolved: https://github.com/pytorch/torchrec/pull/872
    
    Reviewed By: colin2328
    
    Differential Revision: D41846710
    
    Pulled By: YLGH
    
    fbshipit-source-id: e251e1a8ead2ae5b2b38b7c2e7ae357855fb8417

--- setup.py
+++ setup.py
@@ -75,7 +75,17 @@ def main(argv: List[str]) -> None:
 
     print(f"-- {name} building version: {version}")
 
-    packages = find_packages(exclude=("*tests",))
+    packages = find_packages(
+        exclude=(
+            "*tests",
+            "*test",
+            "examples",
+            "*examples.*",
+            "*benchmarks",
+            "*build",
+            "*rfc",
+        )
+    )
     sys.argv = [sys.argv[0]] + unknown
 
     setup(
