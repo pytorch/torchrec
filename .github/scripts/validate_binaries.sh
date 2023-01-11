a3da7960aa3f43d1db724d52c248cb2453ca89d3
atalman <atalman@fb.com>
2023-01-11 12:48:52 -0800

    Refactor using MATRIX vars

--- .github/scripts/validate_binaries.sh
+++ .github/scripts/validate_binaries.sh
@@ -4,23 +4,18 @@
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
-
-
-set -ex
-eval "$(conda shell.bash hook)"
-
-conda create -y -n "${ENV_NAME}" python="${DESIRED_PYTHON}" numpy
+conda create -y -n "${ENV_NAME}" python="${MATRIX_PYTHON_VERSION}" numpy
 conda activate "${ENV_NAME}"
 export PYTORCH_CUDA_PKG=""
 
 # Install pytorch, torchrec and fbgemm as per
 # installation instructions on following page
 # https://github.com/pytorch/torchrec#installations
-if [[ ${GPU_ARCH_TYPE} = 'cuda' ]]; then
-    export PYTORCH_CUDA_PKG="pytorch-cuda=${GPU_ARCH_VER}"
+if [[ ${MATRIX_GPU_ARCH_TYPE} = 'cuda' ]]; then
+    export PYTORCH_CUDA_PKG="pytorch-cuda=${MATRIX_GPU_ARCH_VERSION}"
 fi
 
-if [[ ${CHANNEL} = 'nightly' ]]; then
+if [[ ${MATRIX_CHANNEL} = 'nightly' ]]; then
     # shellcheck disable=SC2086
     conda install -y pytorch ${PYTORCH_CUDA_PKG} -c pytorch-nightly -c nvidia
     pip install torchrec_nightly
@@ -30,8 +25,8 @@ else
     pip install torchrec
 fi
 
-if [[ ${GPU_ARCH_TYPE} = 'cpu' || ${GPU_ARCH_TYPE} = 'rocm' ]]; then
-    if [[ ${CHANNEL} = 'nightly' ]]; then
+if [[ ${MATRIX_GPU_ARCH_TYPE} = 'cpu' || ${MATRIX_GPU_ARCH_TYPE} = 'rocm' ]]; then
+    if [[ ${MATRIX_CHANNEL} = 'nightly' ]]; then
         pip uninstall fbgemm-gpu-nightly -y
         pip install fbgemm-gpu-nightly-cpu
     else
@@ -44,7 +39,7 @@ fi
 python -c "import torch; import fbgemm_gpu; import torchrec"
 
 # Finally run smoke test
-if [[ ${GPU_ARCH_TYPE} = 'cuda' ]]; then
+if [[ ${MATRIX_GPU_ARCH_TYPE} = 'cuda' ]]; then
     torchx run -s local_cwd dist.ddp -j 1 --gpu 2 --script test_installation.py
 else
     torchx run -s local_cwd dist.ddp -j 1 --script test_installation.py -- --cpu_only
