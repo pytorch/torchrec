f0a72041cb9708a5fbbbe8bde202158f0d23c6aa
Dmytro Ivchenko <divchenko@fireworks.ai>
2023-01-23 23:34:12 -0800

    Sharding plan pretty printing (#946)
    Summary:
    Example output: https://gist.github.com/divchenko/6e9526ed7829d2fd5cf7afb2384509c2
    
    We separate parameter sharding and shards tables as it's possible to have several shards per parameter.
    
    Pull Request resolved: https://github.com/pytorch/torchrec/pull/946
    
    Reviewed By: lequytra
    
    Differential Revision: D42641412
    
    Pulled By: YLGH
    
    fbshipit-source-id: 4f577de069d57577a77c9a7d2c4a6a7607d48609

--- torchrec/distributed/types.py
+++ torchrec/distributed/types.py
@@ -447,7 +447,43 @@ class ShardingPlan:
         return self.plan.get(module_path, None)
 
     def __str__(self) -> str:
-        return str(self.plan)
+        from tabulate import tabulate
+
+        out = ""
+        for i, (module_path, module_plan) in enumerate(self.plan.items()):
+            if i > 0:
+                out += "\n\n"
+            out += "module: " + module_path
+            param_table = []
+            shard_table = []
+            for param_name, param_sharding in module_plan.items():
+                param_table.append(
+                    [
+                        param_name,
+                        param_sharding.sharding_type,
+                        param_sharding.compute_kernel,
+                        param_sharding.ranks,
+                    ]
+                )
+                if isinstance(param_sharding.sharding_spec, EnumerableShardingSpec):
+                    shards = param_sharding.sharding_spec.shards
+                    if shards is not None:
+                        for shard in shards:
+                            shard_table.append(
+                                [
+                                    param_name,
+                                    shard.shard_offsets,
+                                    shard.shard_sizes,
+                                    shard.placement,
+                                ]
+                            )
+            out += "\n\n" + tabulate(
+                param_table, ["param", "sharding type", "compute kernel", "ranks"]
+            )
+            out += "\n\n" + tabulate(
+                shard_table, ["param", "shard offsets", "shard sizes", "placement"]
+            )
+        return out
 
 
 ShardedModuleContext = Multistreamable
