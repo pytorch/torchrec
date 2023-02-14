d5dd6dd40bdd0e7c049bad89dfc80963dcdf0240
Sami Stephen Wilf <samiw1@meta.com>
2023-02-14 12:58:16 -0800

    Get Criteo Kaggle dataset working with TorchRec-based DLRM (#1028)
    Summary:
    Pull Request resolved: https://github.com/pytorch/torchrec/pull/1028
    
    X-link: https://github.com/facebookresearch/dlrm/pull/338
    
    Get Criteo Kaggle dataset working with TorchRec-based DLRM
    
    Reviewed By: colin2328
    
    Differential Revision: D43161492
    
    fbshipit-source-id: 1602005b236efe1c3bf65c165c509fd6f22040be

--- torchrec/datasets/scripts/npy_preproc_criteo.py
+++ torchrec/datasets/scripts/npy_preproc_criteo.py
@@ -23,14 +23,22 @@ def parse_args(argv: List[str]) -> argparse.Namespace:
         "--input_dir",
         type=str,
         required=True,
-        help="Input directory containing Criteo tsv files. Files in the directory "
-        "should be named day_{0-23}.",
+        help="Input directory containing Criteo tsv files."
+        "For criteo_1tb, files in the directory should be named day_{0-23}."
+        "For criteo_kaggle, files in the directory should be train.txt & test.txt.",
     )
     parser.add_argument(
         "--output_dir",
         type=str,
         required=True,
         help="Output directory to store npy files.",
+    )
+    parser.add_argument(
+        "--dataset_name",
+        type=str,
+        choices=["criteo_1tb", "criteo_kaggle"],
+        default="criteo_1tb",
+        help="dataset for experiment, current support criteo_1tb, criteo_kaggle",
     )
     return parser.parse_args(argv)
 
@@ -51,13 +59,21 @@ def main(argv: List[str]) -> None:
     input_dir = args.input_dir
     output_dir = args.output_dir
 
-    for i in range(24):
-        in_file_path = os.path.join(input_dir, f"day_{i}")
+    if args.dataset_name == "criteo_1tb":
+        in_files_l = [f"day_{i}" for i in range(24)]
+        out_files_l = in_files_l
+    else:
+        # criteo_kaggle code path
+        in_files_l = ["train.txt", "test.txt"]
+        out_files_l = ["train", "test"]
+
+    for input, output in zip(in_files_l, out_files_l):
+        in_file_path = os.path.join(input_dir, input)
         if not os.path.exists(in_file_path):
             continue
-        dense_out_file_path = os.path.join(output_dir, f"day_{i}_dense.npy")
-        sparse_out_file_path = os.path.join(output_dir, f"day_{i}_sparse.npy")
-        labels_out_file_path = os.path.join(output_dir, f"day_{i}_labels.npy")
+        dense_out_file_path = os.path.join(output_dir, output + "_dense.npy")
+        sparse_out_file_path = os.path.join(output_dir, output + "_sparse.npy")
+        labels_out_file_path = os.path.join(output_dir, output + "_labels.npy")
         print(
             f"Processing {in_file_path}.\nOutput will be saved to\n{dense_out_file_path}"
             f"\n{sparse_out_file_path}\n{labels_out_file_path}"
@@ -67,6 +83,7 @@ def main(argv: List[str]) -> None:
             dense_out_file_path,
             sparse_out_file_path,
             labels_out_file_path,
+            args.dataset_name,
         )
         print(f"Done processing {in_file_path}.")
 
