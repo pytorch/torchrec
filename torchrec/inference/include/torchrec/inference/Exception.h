9cbddeb41f534a17cd315952b8b00facf65b5df5
Yangmei Lin <yangmeilin@meta.com>
2023-03-15 21:51:24 -0700

    Convert GPU Overload error to thrift System error (#1080)
    Summary:
    Pull Request resolved: https://github.com/pytorch/torchrec/pull/1080
    
    Create TorcrechException and GPUOverloadException class to better handle exceptions. Specific exception type can be returned for different cases and later convert to thrift level in Sigrid Predictor.
    
    Reviewed By: zyan0
    
    Differential Revision: D44073803
    
    fbshipit-source-id: 0a64756fa1b24b48b96d408220b00cc423ae9097

--- torchrec/inference/include/torchrec/inference/Exception.h
+++ torchrec/inference/include/torchrec/inference/Exception.h
@@ -17,11 +17,35 @@
 
 namespace torchrec {
 
+class TorchrecException : public std::runtime_error {
+ public:
+  explicit TorchrecException(const std::string& error)
+      : std::runtime_error(error) {}
+};
+
+class GPUOverloadException : public TorchrecException {
+ public:
+  explicit GPUOverloadException(const std::string& error)
+      : TorchrecException(error) {}
+};
+
+template <typename ExceptionType>
 void handleRequestException(
     folly::Promise<std::unique_ptr<PredictionResponse>>& promise,
-    const std::string& msg);
+    const std::string& msg) {
+  auto ex = folly::make_exception_wrapper<ExceptionType>(msg);
+  auto response = std::make_unique<PredictionResponse>();
+  response->exception = std::move(ex);
+  promise.setValue(std::move(response));
+}
+
+template <typename ExceptionType>
 void handleBatchException(
     std::vector<RequestContext>& contexts,
-    const std::string& msg);
+    const std::string& msg) {
+  for (auto& context : contexts) {
+    handleRequestException<ExceptionType>(context.promise, msg);
+  }
+}
 
 } // namespace torchrec
