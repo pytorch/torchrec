d4b6c5b51b9c30aad2426269b122a44c8806097b
Rohan Varma <rvarm1@meta.com>
2023-04-13 14:14:07 -0700

    trec_shard + replicate overall compoasble test (#1114)
    Summary:
    Pull Request resolved: https://github.com/pytorch/torchrec/pull/1114
    
    Ensure the following case can work for composable API:
    
    ```
    m = SparseNN()
    shard(m, sharders=...)
    replicate(m, ignored_modules=m.sparse)
    ```
    
    Differential Revision: D44900403
    
    fbshipit-source-id: 69405030eba59bba118ac1b11bae23f8f368fecb

--- torchrec/distributed/composable/tests/test_ddp.py
+++ torchrec/distributed/composable/tests/test_ddp.py
@@ -11,6 +11,8 @@ import os
 import tempfile
 import unittest
 import uuid
+from typing import List, Tuple
+from copy import deepcopy
 
 import torch
 from torch import distributed as dist
@@ -23,18 +25,31 @@ from torch.distributed.checkpoint import (
     save_state_dict,
 )
 from torch.distributed.launcher.api import elastic_launch, LaunchConfig
-from torchrec.distributed.shard import shard as trec_shard
+from torchrec.distributed.shard import shard as trec_shard, shard_modules
 from torchrec.distributed.sharding_plan import column_wise
 from torchrec.distributed.test_utils.test_model import ModelInput, TestSparseNN
 from torchrec.modules.embedding_configs import EmbeddingBagConfig
 from torchrec.test_utils import skip_if_asan
 
+SEED = 42
+
 
 class DDPTest(unittest.TestCase):
     @classmethod
-    def _run(cls, path: str) -> None:
+    def _get_module(
+        cls, path: str
+    ) -> Tuple[
+        TestSparseNN,
+        torch.device,
+        int,
+        int,
+        List[EmbeddingBagConfig],
+        List[EmbeddingBagConfig],
+    ]:
         rank = int(os.environ["LOCAL_RANK"])
         world_size = int(os.environ["WORLD_SIZE"])
+        torch.manual_seed(SEED)
+        torch.cuda.manual_seed(SEED)
         if torch.cuda.is_available():
             device: torch.device = torch.device(f"cuda:{rank}")
             backend = "nccl"
@@ -42,7 +57,9 @@ class DDPTest(unittest.TestCase):
         else:
             device: torch.device = torch.device("cpu")
             backend = "gloo"
-        dist.init_process_group(backend=backend)
+
+        if not torch.distributed.is_initialized():
+            dist.init_process_group(backend=backend)
         num_float_features = 32
 
         tables = [
@@ -69,6 +86,57 @@ class DDPTest(unittest.TestCase):
             weighted_tables=weighted_tables,
             dense_device=device,
         )
+        return m, device, world_size, num_float_features, tables, weighted_tables
+
+    @classmethod
+    def _run_compose_test(cls, path: str) -> None:
+        (
+            m,
+            device,
+            world_size,
+            num_float_features,
+            tables,
+            weighted_tables,
+        ) = cls._get_module(path)
+        shard_modules(m)
+        replicate(m, ignored_modules=[m.sparse])
+        ######## run one iteration ########
+        _, local_batch = ModelInput.generate(
+            batch_size=8,
+            world_size=world_size,
+            num_float_features=num_float_features,
+            tables=tables,
+            weighted_tables=weighted_tables,
+        )
+        batch = local_batch[0].to(device)
+        batch_local = deepcopy(batch)
+        m(batch)[1].sum().backward()
+
+        # run local model
+        (
+            m_local,
+            device,
+            world_size,
+            num_float_features,
+            tables,
+            weighted_tables,
+        ) = cls._get_module(path)
+        m_local.cuda()
+        m_local(batch_local)[1].sum().backward()
+        # Verify grads are equal
+        for p, p_local in zip(m.parameters(), m_local.parameters()):
+            assert p.sum() == p_local.sum()
+
+    @classmethod
+    def _run_checkpoint_test(cls, path: str) -> None:
+        (
+            m,
+            device,
+            world_size,
+            num_float_features,
+            tables,
+            weighted_tables,
+        ) = cls._get_module(path)
         m.sparse.ebc = trec_shard(
             module=m.sparse.ebc,
             device=device,
@@ -129,7 +197,7 @@ class DDPTest(unittest.TestCase):
         torch.cuda.device_count() <= 1,
         "Not enough GPUs, this test requires at least two GPUs",
     )
-    def test_checkpoint(self) -> None:
+    def _test_checkpoint(self) -> None:
         with tempfile.TemporaryDirectory() as tmpdir, tempfile.TemporaryDirectory() as path:
             lc = LaunchConfig(
                 min_nodes=1,
@@ -143,4 +211,31 @@ class DDPTest(unittest.TestCase):
                 monitor_interval=1,
                 max_restarts=0,
             )
-            elastic_launch(config=lc, entrypoint=self._run)(path)
+            elastic_launch(config=lc, entrypoint=self._run_checkpoint_test)(path)
+
+    @skip_if_asan
+    # pyre-fixme[56]: Pyre was not able to infer the type of argument
+    #  `torch.cuda.device_count() <= 1` to decorator factory `unittest.skipIf`.
+    @unittest.skipIf(
+        torch.cuda.device_count() <= 1,
+        "Not enough GPUs, this test requires at least two GPUs",
+    )
+    def test_replicate_trec_shard_composes(self) -> None:
+        """
+        Ensure that trec_shard followed by replicate results in sparse layers being sharded and
+        dense layers being replicated.
+        """
+        with tempfile.TemporaryDirectory() as tmpdir, tempfile.TemporaryDirectory() as path:
+            lc = LaunchConfig(
+                min_nodes=1,
+                max_nodes=1,
+                nproc_per_node=2,
+                run_id=str(uuid.uuid4()),
+                rdzv_backend="c10d",
+                rdzv_endpoint=os.path.join(tmpdir, "rdzv"),
+                rdzv_configs={"store_type": "file"},
+                start_method="spawn",
+                monitor_interval=1,
+                max_restarts=0,
+            )
+            elastic_launch(config=lc, entrypoint=self._run_compose_test)(path)
