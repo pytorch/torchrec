acfa79daf0b2be10bbbdb298c4a98fa2947496bf
Shabab Ayub <shababayub@meta.com>
2023-05-18 07:18:37 -0700

    Revert D45377649: Multisect successfully blamed D45006690 for test or build failures
    Summary:
    This diff is reverting D45377649
    Depends on D45802223
    D45006690: Zero allocation torchrec sharding via meta [QEBC] by s4ayub has been identified to be causing the following test or build failures:
    
    Tests affected:
    - [dper_lib/silvertorch/delayed_modules/wrapper_modules/tests:build_prospector_model_test - test_ig_prospector_with_atm_publish (dper_lib.silvertorch.delayed_modules.wrapper_modules.tests.build_prospector_model_test.TestBuildProspectorModel)](https://www.internalfb.com/intern/test/281475065803150/)
    
    Here's the Multisect link:
    https://www.internalfb.com/multisect/2047271
    Here are the tasks that are relevant to this breakage:
    
    We're generating a revert to back out the changes in this diff, please note the backout may land if someone accepts it.
    
    If you believe this diff has been generated in error you may Commandeer and Abandon it.
    
    Reviewed By: pengtxiafb
    
    Differential Revision: D45802234
    
    fbshipit-source-id: 9f1597d2c64821aa1a500c076b1b5834f6f442c8

--- torchrec/distributed/test_utils/infer_utils.py
+++ torchrec/distributed/test_utils/infer_utils.py
@@ -213,11 +213,7 @@ class TestQuantEBCSharder(QuantEmbeddingBagCollectionSharder):
 
 
 class TestQuantECSharder(QuantEmbeddingCollectionSharder):
-    def __init__(
-        self,
-        sharding_type: str,
-        kernel_type: str,
-    ) -> None:
+    def __init__(self, sharding_type: str, kernel_type: str) -> None:
         super().__init__()
         self._sharding_type = sharding_type
         self._kernel_type = kernel_type
@@ -242,9 +238,7 @@ class TestQuantECSharder(QuantEmbeddingCollectionSharder):
             dtype_to_data_type(module.output_dtype())
         )
         fused_params[FUSED_PARAM_REGISTER_TBE_BOOL] = True
-        return ShardedQuantEmbeddingCollection(
-            module, params, env, fused_params, device
-        )
+        return ShardedQuantEmbeddingCollection(module, params, env, fused_params)
 
 
 class KJTInputWrapper(torch.nn.Module):
