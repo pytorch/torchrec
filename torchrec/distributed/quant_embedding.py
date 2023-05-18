e0a5c4a9556ca00fb17c2c3475cadad2365f08e8
Shabab Ayub <shababayub@meta.com>
2023-05-18 07:50:21 -0700

    Revert D45377649: Multisect successfully blamed D45006690 for test or build failures
    Summary:
    This diff is reverting D45377649
    Depends on D45802223
    D45006690: Zero allocation torchrec sharding via meta [QEBC] by s4ayub has been identified to be causing the following test or build failures:
    
    Tests affected:
    - [dper_lib/silvertorch/delayed_modules/wrapper_modules/tests:build_prospector_model_test - test_ig_prospector_with_atm_publish (dper_lib.silvertorch.delayed_modules.wrapper_modules.tests.build_prospector_model_test.TestBuildProspectorModel)](https://www.internalfb.com/intern/test/281475065803150/)
    
    Here's the Multisect link:
    https://www.internalfb.com/multisect/2047271
    Here are the tasks that are relevant to this breakage:
    
    We're generating a revert to back out the changes in this diff, please note the backout may land if someone accepts it.
    
    If you believe this diff has been generated in error you may Commandeer and Abandon it.
    
    Reviewed By: pengtxiafb
    
    Differential Revision: D45802234
    
    fbshipit-source-id: 9dd4e805f9f2d1abcb6bfd8e09e539b9a95bc2ff

--- torchrec/distributed/quant_embedding.py
+++ torchrec/distributed/quant_embedding.py
@@ -156,7 +156,6 @@ class ShardedQuantEmbeddingCollection(
         table_name_to_parameter_sharding: Dict[str, ParameterSharding],
         env: ShardingEnv,
         fused_params: Optional[Dict[str, Any]] = None,
-        device: Optional[torch.device] = None,
     ) -> None:
         super().__init__()
 
@@ -180,10 +179,9 @@ class ShardedQuantEmbeddingCollection(
             for sharding_type, embedding_confings in sharding_type_to_sharding_infos.items()
         }
 
-        self._device = device
         self._input_dists: List[nn.Module] = []
         self._lookups: List[nn.Module] = []
-        self._create_lookups(fused_params, device)
+        self._create_lookups(fused_params)
         self._output_dists: List[nn.Module] = []
 
         self._feature_splits: List[int] = []
@@ -194,8 +192,6 @@ class ShardedQuantEmbeddingCollection(
 
         self._embedding_dim: int = module.embedding_dim()
         self._need_indices: bool = module.need_indices()
-
-        self._fused_params = fused_params
 
         # This provides consistency between this class and the EmbeddingBagCollection's
         # nn.Module API calls (state_dict, named_modules, etc)
@@ -230,14 +226,11 @@ class ShardedQuantEmbeddingCollection(
         self,
         input_feature_names: List[str],
         device: torch.device,
-        input_dist_device: Optional[torch.device] = None,
     ) -> None:
         feature_names: List[str] = []
         self._feature_splits: List[int] = []
         for sharding in self._sharding_type_to_sharding.values():
-            self._input_dists.append(
-                sharding.create_input_dist(device=input_dist_device)
-            )
+            self._input_dists.append(sharding.create_input_dist())
             feature_names.extend(sharding.feature_names())
             self._feature_splits.append(len(sharding.feature_names()))
         self._features_order: List[int] = []
@@ -254,15 +247,9 @@ class ShardedQuantEmbeddingCollection(
             persistent=False,
         )
 
-    def _create_lookups(
-        self,
-        fused_params: Optional[Dict[str, Any]],
-        device: Optional[torch.device] = None,
-    ) -> None:
+    def _create_lookups(self, fused_params: Optional[Dict[str, Any]]) -> None:
         for sharding in self._sharding_type_to_sharding.values():
-            self._lookups.append(
-                sharding.create_lookup(fused_params=fused_params, device=device)
-            )
+            self._lookups.append(sharding.create_lookup(fused_params=fused_params))
 
     def _create_output_dist(
         self,
@@ -282,7 +269,6 @@ class ShardedQuantEmbeddingCollection(
             self._create_input_dist(
                 input_feature_names=features.keys() if features is not None else [],
                 device=features.device(),
-                input_dist_device=self._device,
             )
             self._has_uninitialized_input_dist = False
         if self._has_uninitialized_output_dist:
