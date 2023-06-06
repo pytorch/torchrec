52ac150ddb2b75803810b1f3d906df8ac01d4620
Henry Tsang <henrylhtsang@meta.com>
2023-06-06 15:07:08 -0700

    Update pyre to a newer version (#1200)
    Summary:
    Pull Request resolved: https://github.com/pytorch/torchrec/pull/1200
    
    Update pyre to a newer version.
    Should fix the error `ERROR: Could not find a version that satisfies the requirement pyre-check-nightly==0.0.101683630711`
    
    Added two pyre-ignores as well.
    
    Differential Revision: D46496954
    
    fbshipit-source-id: f1e43d84f65d45565e4d25df2c9cba270a06720e

--- torchrec/modules/lazy_extension.py
+++ torchrec/modules/lazy_extension.py
@@ -164,7 +164,7 @@ class LazyModuleExtensionMixin(LazyModuleMixin):
         return super().apply(fn)
 
     # fmt: off
-    # pyre-ignore[2, 47]
+    # pyre-ignore
     def _infer_parameters(self: _LazyExtensionProtocol, module, input, kwargs) -> None:
         r"""Infers the size and initializes the parameters according to the
         provided input batch.
@@ -255,5 +255,5 @@ class LazyModuleExtensionMixin(LazyModuleMixin):
         return result
     # fmt: on
 
-    # pyre-ignore[4]
+    # pyre-ignore
     __call__: Callable[..., Any] = _call_impl
