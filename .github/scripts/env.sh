7a5f55d32d0ac1c1a040084921dfa72d4677b461
henrylhtsang <91030427+henrylhtsang@users.noreply.github.com>
2023-06-29 13:29:24 -0700

    Update env.sh

--- .github/scripts/env.sh
+++ .github/scripts/env.sh
@@ -5,6 +5,8 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+conda install -n test -y cuda -c "nvidia/label/cuda-11.8.0"
+
 echo "before LD library path"
 echo $LD_LIBRARY_PATH
 echo "after LD library path"
