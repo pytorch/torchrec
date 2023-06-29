be2fae38b11c48254dbc479f8da5a0f42ecb3a74
Henry Tsang <henrylhtsang@meta.com>
2023-06-28 20:45:15 -0700

    testing github action (#1243)
    Summary: Pull Request resolved: https://github.com/pytorch/torchrec/pull/1243
    
    Differential Revision: D46978237
    
    fbshipit-source-id: 4010e6b61bc2de20bd9e08aed1155b3f856ce3e8

---
+++ .github/scripts/env.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+echo "before LD library path"
+echo $LD_LIBRARY_PATH
+echo "after LD library path"
+
+
+
+echo "before ldconfig"
+sudo ldconfig -p
+echo "after ldconfig"
+
+echo "before nvidia-smi"
+sudo nvidia-smi
+echo "after nvidia-smi"
