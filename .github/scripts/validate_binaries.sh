a50dad6d8ef3a17094d44aba1d843f12fb90f8fa
henrylhtsang <91030427+henrylhtsang@users.noreply.github.com>
2023-07-12 16:42:55 -0700

    Update validate_binaries.sh

--- .github/scripts/validate_binaries.sh
+++ .github/scripts/validate_binaries.sh
@@ -5,7 +5,9 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-conda update --all
+conda info 
+
+conda install conda=23.3.1
 
 export PYTORCH_CUDA_PKG=""
 
