5c5633f65ca900644c249e3222852beefd8ec82e
Ying Liu <yingliufb@meta.com>
2023-07-25 15:37:07 -0700

    Make FP Embedding Modules FX + Scriptable (#1285)
    Summary:
    Pull Request resolved: https://github.com/pytorch/torchrec/pull/1285
    
    ATT
    
    Mostly massaging code, and depending on inputs in constructor (embedding table names), rather than kt.keys()
    
    Reviewed By: yuchenhao
    
    Differential Revision: D47598549
    
    fbshipit-source-id: abd1e60be031cd56cb98af8c0582e62e81f801b5

--- torchrec/modules/tests/test_feature_processor_.py
+++ torchrec/modules/tests/test_feature_processor_.py
@@ -8,7 +8,12 @@
 import unittest
 
 import torch
-from torchrec.modules.feature_processor_ import PositionWeightedModule
+
+from torchrec.fx.tracer import symbolic_trace
+from torchrec.modules.feature_processor_ import (
+    PositionWeightedModule,
+    PositionWeightedModuleCollection,
+)
 from torchrec.sparse.jagged_tensor import KeyedJaggedTensor
 
 
@@ -30,7 +35,6 @@ class PositionWeightedModuleTest(unittest.TestCase):
 
         jt = features["f1"]
         weighted_features = pw(jt)
-        print("weighted features", weighted_features)
 
         self.assertEqual(weighted_features.weights().size(), (3,))
 
@@ -40,3 +44,50 @@ class PositionWeightedModuleTest(unittest.TestCase):
 
         pw_f1 = weighted_features.weights().detach()
         self.assertTrue(torch.allclose(pw_f1_ref, pw_f1))
+
+        position_weighted_module_gm = symbolic_trace(pw)
+        position_weighted_module_gm_script = torch.jit.script(
+            position_weighted_module_gm
+        )
+
+        weighted_features_gm_script = position_weighted_module_gm_script(jt)
+        torch.testing.assert_close(
+            weighted_features.values(), weighted_features_gm_script.values()
+        )
+        torch.testing.assert_close(
+            weighted_features.lengths(), weighted_features_gm_script.lengths()
+        )
+
+
+class PositionWeightedCollectionModuleTest(unittest.TestCase):
+    def test_populate_weights(self) -> None:
+        position_weighted_module_collection = PositionWeightedModuleCollection(
+            {"f1": 10, "f2": 10}
+        )
+
+        #     0       1        2  <-- batch
+        # 0   [0,1] None    [2]
+        # 1   [3]    [4]    [5,6,7]
+        # ^
+        # feature
+        features = KeyedJaggedTensor.from_offsets_sync(
+            keys=["f1", "f2"],
+            values=torch.tensor([0, 1, 2, 3, 4, 5, 6, 7]),
+            offsets=torch.tensor([0, 2, 2, 3, 4, 5, 8]),
+        )
+
+        fp_kjt = position_weighted_module_collection(features)
+
+        position_weighted_module_collection_gm = symbolic_trace(
+            position_weighted_module_collection
+        )
+        position_weighted_module_collection_gm_script = torch.jit.script(
+            position_weighted_module_collection_gm
+        )
+        fp_kjt_gm_script = position_weighted_module_collection_gm_script(features)
+
+        torch.testing.assert_close(fp_kjt.values(), fp_kjt_gm_script.values())
+        torch.testing.assert_close(fp_kjt.lengths(), fp_kjt_gm_script.lengths())
+        torch.testing.assert_close(
+            fp_kjt.length_per_key(), fp_kjt_gm_script.length_per_key()
+        )
