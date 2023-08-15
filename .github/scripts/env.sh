ca9098baba679f76ce4aa96ce522e878bad902ee
Omkar Salpekar <osalpekar@fb.com>
2023-08-14 17:21:53 -0700

    Try to Fix Nova Release Pipelines

---
+++ .github/scripts/env.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# conda install -y cuda -c "nvidia/label/cuda-11.8.0"
+# conda install -y -c conda-forge cudatoolkit=11.2 cudnn=8.1.0
+
+# echo "before LD library path"
+# echo $LD_LIBRARY_PATH
+# echo "after LD library path"
+
+echo "before ldconfig"
+{
+ldconfig -p && output=$(ldconfig -p); echo "$output"
+} || {
+echo "ldconfig failed"t
+}
+echo "after ldconfig"
+
+echo "before nvidia-smi"
+{
+sudo nvidia-smi
+} || {
+nvidia-smi
+} || {
+echo "nvidia-smi failed"
+}
+echo "after nvidia-smi"
+
+# echo "before cuda"
+# nvcc --version
+# echo "after cuda"
