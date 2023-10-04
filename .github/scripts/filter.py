79f8d4960affd9c287c4b7568ab5940ff37d58c0
Henry Tsang <henrylhtsang@fb.com>
2023-10-04 09:55:59 -0700

    Update install methods after fbgemm onboarded onto nova (#1420)
    Summary:
    
    Update the install methods now that fbgemm has onboarded project nova.
    
    Also this publishes cuda 12.1 as well.
    
    Also updating gpu ci, even though it is currently not used.
    
    Differential Revision: D49852074

--- .github/scripts/filter.py
+++ .github/scripts/filter.py
@@ -22,8 +22,7 @@ def main():
     new_matrix_entries = []
 
     for entry in full_matrix["include"]:
-        if entry["gpu_arch_version"] != "12.1":
-            new_matrix_entries.append(entry)
+        new_matrix_entries.append(entry)
 
     new_matrix = {"include": new_matrix_entries}
     print(json.dumps(new_matrix))
