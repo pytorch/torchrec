8e975d22c7a893285626436934b0363796039c0f
Paul Zhang <paulzhan@meta.com>
2024-02-15 08:50:16 -0800

    Introduce Training benchmarks (#1705)
    Summary: Pull Request resolved: https://github.com/pytorch/torchrec/pull/1705
    
    Differential Revision: D53481057

--- torchrec/distributed/benchmark/benchmark_train.py
+++ torchrec/distributed/benchmark/benchmark_train.py
@@ -8,11 +8,12 @@
 #!/usr/bin/env python3
 
 import argparse
+import copy
 import logging
 import os
 import time
 from functools import partial
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 
 import torch
 
@@ -27,14 +28,11 @@ from torchrec.distributed.benchmark.benchmark_utils import (
     write_report,
 )
 from torchrec.distributed.embedding_types import EmbeddingComputeKernel, ShardingType
-from torchrec.distributed.test_utils.infer_utils import (
-    TestQuantEBCSharder,
-    TestQuantECSharder,
-)
-from torchrec.quant.embedding_modules import (
-    EmbeddingBagCollection as QuantEmbeddingBagCollection,
-    EmbeddingCollection as QuantEmbeddingCollection,
-)
+from torchrec.distributed.test_utils.test_model import TestEBCSharder
+from torchrec.distributed.types import DataType
+from torchrec.modules.embedding_modules import EmbeddingBagCollection
+from torchrec.sparse.jagged_tensor import KeyedJaggedTensor
+
 
 logger: logging.Logger = logging.getLogger()
 
@@ -47,9 +45,8 @@ BENCH_SHARDING_TYPES: List[ShardingType] = [
 
 BENCH_COMPILE_MODES: List[CompileMode] = [
     CompileMode.EAGER,
-    CompileMode.FX_SCRIPT,
+    # CompileMode.FX_SCRIPT,
 ]
-
 
 TABLE_SIZES: List[Tuple[int, int]] = [
     (num_embeddings, EMBEDDING_DIM)
@@ -57,55 +54,39 @@ TABLE_SIZES: List[Tuple[int, int]] = [
 ]
 
 
-def benchmark_qec(args: argparse.Namespace, output_dir: str) -> List[BenchmarkResult]:
-    tables = get_tables(TABLE_SIZES, is_pooled=False)
-    sharder = TestQuantECSharder(
-        sharding_type="",
-        kernel_type=EmbeddingComputeKernel.QUANT.value,
-        shardable_params=[table.name for table in tables],
+def training_func_to_benchmark(
+    model: torch.nn.Module,
+    bench_inputs: List[KeyedJaggedTensor],
+    optimizer: Optional[torch.optim.Optimizer],
+) -> None:
+    for bench_input in bench_inputs:
+        pooled_embeddings = model(bench_input)
+        vals = []
+        for _name, param in pooled_embeddings.to_dict().items():
+            vals.append(param)
+        torch.cat(vals, dim=1).sum().backward()
+        if optimizer:
+            optimizer.step()
+            optimizer.zero_grad()
+
+
+def benchmark_ebc(
+    tables: List[Tuple[int, int]], args: argparse.Namespace, output_dir: str
+) -> List[BenchmarkResult]:
+    table_configs = get_tables(tables, data_type=DataType.FP32)
+    sharder = TestEBCSharder(
+        sharding_type="",  # sharding_type gets populated during benchmarking
+        kernel_type=EmbeddingComputeKernel.DENSE.value,
     )
 
-    module = QuantEmbeddingCollection(
+    module = EmbeddingBagCollection(
         # pyre-ignore [6]
-        tables=tables,
-        device=torch.device("cpu"),
-        quant_state_dict_split_scale_bias=True,
-    )
-
-    args_kwargs = {
-        argname: getattr(args, argname)
-        for argname in dir(args)
-        # Don't include output_dir since output_dir was modified
-        if not argname.startswith("_") and argname != "output_dir"
-    }
-
-    return benchmark_module(
-        module=module,
-        sharder=sharder,
-        sharding_types=BENCH_SHARDING_TYPES,
-        compile_modes=BENCH_COMPILE_MODES,
-        tables=tables,
-        output_dir=output_dir,
-        **args_kwargs,
-    )
-
-
-def benchmark_qebc(args: argparse.Namespace, output_dir: str) -> List[BenchmarkResult]:
-    tables = get_tables(TABLE_SIZES)
-    sharder = TestQuantEBCSharder(
-        sharding_type="",
-        kernel_type=EmbeddingComputeKernel.QUANT.value,
-        shardable_params=[table.name for table in tables],
-    )
-
-    module = QuantEmbeddingBagCollection(
-        # pyre-ignore [6]
-        tables=tables,
+        tables=table_configs,
         is_weighted=False,
         device=torch.device("cpu"),
-        quant_state_dict_split_scale_bias=True,
     )
 
+    optimizer = torch.optim.SGD(module.parameters(), lr=0.02)
     args_kwargs = {
         argname: getattr(args, argname)
         for argname in dir(args)
@@ -118,8 +99,10 @@ def benchmark_qebc(args: argparse.Namespace, output_dir: str) -> List[BenchmarkR
         sharder=sharder,
         sharding_types=BENCH_SHARDING_TYPES,
         compile_modes=BENCH_COMPILE_MODES,
-        tables=tables,
+        tables=table_configs,
         output_dir=output_dir,
+        func_to_benchmark=training_func_to_benchmark,
+        benchmark_func_kwargs={"optimizer": optimizer},
         **args_kwargs,
     )
 
@@ -137,24 +120,28 @@ def main() -> None:
 
     benchmark_results_per_module = []
     write_report_funcs_per_module = []
+    shrunk_table_sizes = []
 
-    for module_name in ["QuantEmbeddingBagCollection", "QuantEmbeddingCollection"]:
-        output_dir = args.output_dir + f"/run_{datetime_sfx}"
-        if module_name == "QuantEmbeddingBagCollection":
-            output_dir += "_qebc"
-            benchmark_func = benchmark_qebc
+    for i in range(len(TABLE_SIZES)):
+        if TABLE_SIZES[i][0] > 1000000:
+            shrunk_table_sizes.append((1000000, TABLE_SIZES[i][1]))
         else:
-            output_dir += "_qec"
-            benchmark_func = benchmark_qec
+            shrunk_table_sizes.append(TABLE_SIZES[i])
+
+    for module_name in ["EmbeddingBagCollection"]:
+        output_dir = args.output_dir + f"/run_{datetime_sfx}"
+        output_dir += "_ebc"
+        benchmark_func = benchmark_ebc
 
         if not os.path.exists(output_dir):
             # Place all outputs under the datetime folder
             os.mkdir(output_dir)
 
-        tables_info = "\nTABLE SIZES QUANT:"
-        for i, (num, dim) in enumerate(TABLE_SIZES):
-            mb = int(float(num * dim) / 1024 / 1024)
-            tables_info += f"\nTABLE[{i}][{num:9}, {dim:4}] u8: {mb:6}Mb"
+        tables_info = "\nTABLE SIZES:"
+        for i, (num, dim) in enumerate(shrunk_table_sizes):
+            # FP32 is 4 bytes
+            mb = int(float(num * dim) / 1024 / 1024) * 4
+            tables_info += f"\nTABLE[{i}][{num:9}, {dim:4}] {mb:6}Mb"
 
         report: str = f"REPORT BENCHMARK {datetime_sfx} world_size:{args.world_size} batch_size:{args.batch_size}\n"
         report += f"Module: {module_name}\n"
@@ -165,7 +152,9 @@ def main() -> None:
         report_file: str = f"{output_dir}/run.report"
 
         # Save results to output them once benchmarking is all done
-        benchmark_results_per_module.append(benchmark_func(args, output_dir))
+        benchmark_results_per_module.append(
+            benchmark_func(shrunk_table_sizes, args, output_dir)
+        )
         write_report_funcs_per_module.append(
             partial(
                 write_report,
