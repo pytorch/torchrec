9507b1176bde97886967442b80336076220e409d
Henry Tsang <henrylhtsang@meta.com>
2024-02-20 18:09:09 -0800

    Add test_load_state_dict for FP-ebc (#1709)
    Summary:
    
    Add test_load_state_dict for FP-ebc.
    
    Reviewed By: sarckk
    
    Differential Revision: D53839126

--- torchrec/distributed/tests/test_fp_embeddingbag_utils.py
+++ torchrec/distributed/tests/test_fp_embeddingbag_utils.py
@@ -7,10 +7,15 @@
 
 #!/usr/bin/env python3
 
-from typing import cast, Dict, List, Tuple
+from typing import Any, cast, Dict, List, Optional, Tuple
 
 import torch
 from torch import nn
+from torchrec.distributed.fp_embeddingbag import (
+    FeatureProcessedEmbeddingBagCollectionSharder,
+)
+from torchrec.distributed.test_utils.test_model import TestEBCSharder
+from torchrec.distributed.types import QuantizedCommCodecs
 from torchrec.modules.embedding_configs import EmbeddingBagConfig
 from torchrec.modules.embedding_modules import EmbeddingBagCollection
 from torchrec.modules.feature_processor_ import (
@@ -86,3 +91,131 @@ def create_module_and_freeze(
     torch.manual_seed(0)
 
     return sparse_arch
+
+
+class TestFPEBCSharder(FeatureProcessedEmbeddingBagCollectionSharder):
+    def __init__(
+        self,
+        sharding_type: str,
+        kernel_type: str,
+        fused_params: Optional[Dict[str, Any]] = None,
+        qcomm_codecs_registry: Optional[Dict[str, QuantizedCommCodecs]] = None,
+    ) -> None:
+        if fused_params is None:
+            fused_params = {}
+
+        self._sharding_type = sharding_type
+        self._kernel_type = kernel_type
+
+        ebc_sharder = TestEBCSharder(
+            self._sharding_type,
+            self._kernel_type,
+            fused_params,
+            qcomm_codecs_registry,
+        )
+        super().__init__(ebc_sharder, qcomm_codecs_registry)
+
+    def sharding_types(self, compute_device_type: str) -> List[str]:
+        """
+        Restricts sharding to single type only.
+        """
+        return (
+            [self._sharding_type]
+            if self._sharding_type
+            in super().sharding_types(compute_device_type=compute_device_type)
+            else []
+        )
+
+    def compute_kernels(
+        self, sharding_type: str, compute_device_type: str
+    ) -> List[str]:
+        """
+        Restricts to single impl.
+        """
+        return [self._kernel_type]
+
+
+def get_configs_and_kjt_inputs() -> Tuple[
+    List[EmbeddingBagConfig], List[KeyedJaggedTensor]
+]:
+    embedding_bag_config = [
+        EmbeddingBagConfig(
+            name="table_0",
+            feature_names=["feature_0"],
+            embedding_dim=3 * 16,
+            num_embeddings=16,
+        ),
+        EmbeddingBagConfig(
+            name="table_1",
+            feature_names=["feature_1"],
+            embedding_dim=8,
+            num_embeddings=16,
+        ),
+        EmbeddingBagConfig(
+            name="table_2",
+            feature_names=["feature_2"],
+            embedding_dim=8,
+            num_embeddings=16,
+        ),
+        EmbeddingBagConfig(
+            name="table_3",
+            feature_names=["feature_3"],
+            embedding_dim=3 * 16,
+            num_embeddings=16,
+        ),
+    ]
+
+    # Rank 0
+    #             instance 0   instance 1  instance 2
+    # "feature_0"   [0, 1]       None        [2]
+    # "feature_1"   [0, 1]       None        [2]
+    # "feature_2"   [3, 1]       [4,1]        [5]
+    # "feature_3"   [1]       [6,1,8]        [0,3,3]
+
+    # Rank 1
+
+    #             instance 0   instance 1  instance 2
+    # "feature_0"   [3, 2]       [1,2]       [0,1,2,3]
+    # "feature_1"   [2, 3]       None        [2]
+    # "feature_2"   [2, 7]       [1,8,2]        [8,1]
+    # "feature_3"   [9]       [8]        [7]
+
+    kjt_input_per_rank = [  # noqa
+        KeyedJaggedTensor.from_lengths_sync(
+            keys=["feature_0", "feature_1", "feature_2", "feature_3"],
+            values=torch.LongTensor(
+                [0, 1, 2, 0, 1, 2, 3, 1, 4, 1, 5, 1, 6, 1, 8, 0, 3, 3]
+            ),
+            lengths=torch.LongTensor(
+                [
+                    2,
+                    0,
+                    1,
+                    2,
+                    0,
+                    1,
+                    2,
+                    2,
+                    1,
+                    1,
+                    3,
+                    3,
+                ]
+            ),
+            weights=torch.FloatTensor(
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            ),
+        ),
+        KeyedJaggedTensor.from_lengths_sync(
+            keys=["feature_0", "feature_1", "feature_2", "feature_3"],
+            values=torch.LongTensor(
+                [3, 2, 1, 2, 0, 1, 2, 3, 2, 3, 2, 2, 7, 1, 8, 2, 8, 1, 9, 8, 7]
+            ),
+            lengths=torch.LongTensor([2, 2, 4, 2, 0, 1, 2, 3, 2, 1, 1, 1]),
+            weights=torch.FloatTensor(
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            ),
+        ),
+    ]
+
+    return embedding_bag_config, kjt_input_per_rank
