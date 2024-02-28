c1ef945c63c737dc82abff73145f75d890ad934a
Shiyan Deng <dsy842974287@meta.com>
2024-02-27 17:40:08 -0800

    add autodep rule for torchrec/inference/Batching.h
    Summary: Allow autodeps to auto add `fbcode//torchrec/inference:batching` to the buck target when we imported `torchrec/inference/Batching.h`
    
    Reviewed By: houseroad
    
    Differential Revision: D54284062

--- torchrec/inference/src/Batching.cpp
+++ torchrec/inference/src/Batching.cpp
@@ -6,7 +6,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-#include "torchrec/inference/Batching.h"
+#include "torchrec/inference/Batching.h" // @manual
 
 #include <c10/core/ScalarType.h>
 #include <folly/Range.h>
