62d0742df51f62854d7c50d7ef6ac26b098da6b4
Henry Tsang <henrylhtsang@meta.com>
2024-03-14 17:13:17 -0700

    dyn col dim proposer (#1794)
    Summary:
    Pull Request resolved: https://github.com/pytorch/torchrec/pull/1794
    
    Refactored version of D48339456.
    
    design doc (wip): https://docs.google.com/document/d/1cM8jlJJJYwkXDfeRKGT9jo_S4IDnVZ0X2DWF2XwHTvE/
    
    The idea is to decrease CW dim for certain tables that are causing max perf or max hbm usage.
    
    Reviewed By: ge0405
    
    Differential Revision: D54824506
    
    fbshipit-source-id: 8fb18c00c89b059f00f63de62f0b435e60f0fe7b

--- torchrec/distributed/planner/perf_models.py
+++ torchrec/distributed/planner/perf_models.py
@@ -9,10 +9,21 @@
 
 from typing import cast, List
 
-from torchrec.distributed.planner.types import Perf, PerfModel, ShardingOption, Topology
+from torchrec.distributed.planner.types import (
+    Perf,
+    PerfModel,
+    ShardingOption,
+    Storage,
+    Topology,
+)
 
 
 class NoopPerfModel(PerfModel):
+    """
+    A no-op model that returns the maximum perf among all shards. Here, no-op
+    means we estimate the performance of a model without actually running it.
+    """
+
     def __init__(self, topology: Topology) -> None:
         self._topology = topology
 
@@ -24,3 +35,22 @@ class NoopPerfModel(PerfModel):
                 perfs[shard.rank] += cast(Perf, shard.perf).total
 
         return max(perfs)
+
+
+class NoopStorageModel(PerfModel):
+    """
+    A no-op model that returns the maximum hbm usage among all shards. Here, no-op
+    means we estimate the performance of a model without actually running it.
+    """
+
+    def __init__(self, topology: Topology) -> None:
+        self._topology = topology
+
+    def rate(self, plan: List[ShardingOption]) -> float:
+        hbms = [0] * self._topology.world_size
+        for sharding_option in plan:
+            for shard in sharding_option.shards:
+                # pyre-ignore [6]: Expected `typing_extensions.SupportsIndex`
+                hbms[shard.rank] += cast(Storage, shard.storage).hbm
+
+        return max(hbms)
