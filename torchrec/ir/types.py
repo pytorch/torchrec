4020e16f753c9d5d197fe63ecbce8184b1e326b6
Paul Zhang <paulzhan@meta.com>
2024-04-05 08:02:37 -0700

    Introduce Serializer in TorchRec for torch.export PEA (#1848)
    Summary:
    Pull Request resolved: https://github.com/pytorch/torchrec/pull/1848
    
    Adding a serializer class to support thrift serialization of PEA configs for regenerating the eager module after torch.export through saving the metadata as a bytes buffer in the module.
    
    Reviewed By: dstaay-fb
    
    Differential Revision: D55661704
    
    fbshipit-source-id: 6ea47e495eaad032f17ad6c5f45206259c8009b3

---
+++ torchrec/ir/types.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+#!/usr/bin/env python3
+
+import abc
+from typing import Any, Dict, Type
+
+from torch import nn
+
+
+class SerializerInterface(abc.ABC):
+    """
+    Interface for Serializer classes for torch.export IR.
+    """
+
+    @classmethod
+    @property
+    # pyre-ignore [3]: Returning `None` but type `Any` is specified.
+    def module_to_serializer_cls(cls) -> Dict[Type[nn.Module], Type[Any]]:
+        raise NotImplementedError
+
+    @classmethod
+    @abc.abstractmethod
+    # pyre-ignore [3]: Returning `None` but type `Any` is specified.
+    def serialize(
+        cls,
+        module: nn.Module,
+    ) -> Any:
+        # Take the eager embedding module and generate bytes in buffer
+        pass
+
+    @classmethod
+    @abc.abstractmethod
+    # pyre-ignore [2]: Parameter `input` must have a type other than `Any`.
+    def deserialize(cls, input: Any) -> nn.Module:
+        # Take the bytes in the buffer and regenerate the eager embedding module
+        pass
