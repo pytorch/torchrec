205f5bafb3a76f3cad9dda2e96273e9a7828889e
Ivan Kobzarev <ivankobzarev@meta.com>
2024-04-22 11:39:48 -0700

    Back out [TorchRec][Planer][Inference] Prototype for uneven sharding autoplanner support, [TorchRec][Fix] fix torch package issue caused by heterogenous planner (#1908)
    Summary:
    Pull Request resolved: https://github.com/pytorch/torchrec/pull/1908
    
    Original commit changeset: 538744068af1
    
    Original Phabricator Diff: D56360099
    ***
    Back out "[TorchRec][Inference] Support auto planner in _shard_modules"
    
    Original commit changeset: 167c92b44bda
    
    Original Phabricator Diff: D55423537
    
    Reviewed By: guangyuwang, gnahzg
    
    Differential Revision: D56426016
    
    fbshipit-source-id: f28f6bc5b527af7cf87b9ac0002cb30cce09ee4b

--- torchrec/distributed/tests/test_infer_hetero_shardings.py
+++ torchrec/distributed/tests/test_infer_hetero_shardings.py
@@ -21,10 +21,7 @@ from torchrec.distributed.embedding_types import ShardingType
 from torchrec.distributed.planner import ParameterConstraints
 from torchrec.distributed.planner.planners import HeteroEmbeddingShardingPlanner
 from torchrec.distributed.planner.types import CustomTopologyData, Topology
-from torchrec.distributed.quant_embedding import (
-    QuantEmbeddingCollectionSharder,
-    ShardedQuantEmbeddingCollection,
-)
+from torchrec.distributed.quant_embedding import QuantEmbeddingCollectionSharder
 from torchrec.distributed.quant_embeddingbag import QuantEmbeddingBagCollectionSharder
 from torchrec.distributed.shard import _shard_modules
 from torchrec.distributed.sharding.rw_sharding import InferCPURwSparseFeaturesDist
@@ -221,11 +218,6 @@ class InferHeteroShardingsTest(unittest.TestCase):
                 == env.world_size
             )
 
-    # pyre-ignore
-    @unittest.skipIf(
-        torch.cuda.device_count() <= 3,
-        "Not enough GPUs available",
-    )
     def test_cpu_gpu_sharding_autoplanner(self) -> None:
         num_embeddings = 10
         emb_dim = 16
@@ -282,168 +274,6 @@ class InferHeteroShardingsTest(unittest.TestCase):
             .type,
             "cpu",
         )
-        self.assertTrue(
-            module_plan.plan["_module_kjt_input.0"]["table_1"]
-            .sharding_spec.shards[0]
-            .placement.device()
-            .type,
-            "cuda",
-        )
-        self.assertTrue(
-            module_plan.plan["_module_kjt_input.0"]["table_2"]
-            .sharding_spec.shards[0]
-            .placement.device()
-            .type,
-            "cuda",
-        )
-
-    # pyre-ignore
-    @unittest.skipIf(
-        torch.cuda.device_count() <= 3,
-        "Not enough GPUs available",
-    )
-    def test_cpu_gpu_sharding_shard_modules(self) -> None:
-        num_embeddings = 10
-        emb_dim = 16
-        tables = [
-            EmbeddingConfig(
-                num_embeddings=num_embeddings,
-                embedding_dim=emb_dim,
-                name=f"table_{i}",
-                feature_names=[f"feature_{i}"],
-            )
-            for i in range(3)
-        ]
-        model = KJTInputWrapper(
-            module_kjt_input=torch.nn.Sequential(
-                EmbeddingCollection(
-                    tables=tables,
-                    device=torch.device("cpu"),
-                )
-            )
-        )
-        non_sharded_model = quantize(
-            model,
-            inplace=False,
-            quant_state_dict_split_scale_bias=True,
-            weight_dtype=torch.qint8,
-        )
-        sharder = QuantEmbeddingCollectionSharder()
-        env_dict = {
-            "cpu": ShardingEnv.from_local(
-                3,
-                0,
-            ),
-            "cuda": ShardingEnv.from_local(
-                2,
-                0,
-            ),
-        }
-
-        shard_model = _shard_modules(
-            module=non_sharded_model,
-            env=env_dict,
-            # pyre-ignore
-            sharders=[sharder],
-            device=torch.device("cpu"),
-        )
-
-        self.assertTrue(
-            isinstance(
-                shard_model._module_kjt_input[0], ShardedQuantEmbeddingCollection
-            )
-        )
-
-        self.assertEqual(len(shard_model._module_kjt_input[0]._lookups), 1)
-        self.assertEqual(
-            len(
-                shard_model._module_kjt_input[0]._lookups[0]._embedding_lookups_per_rank
-            ),
-            env_dict["cpu"].world_size,
-        )
-
-    def test_cpu_gpu_uneven_sharding_shard_modules(self) -> None:
-        num_embeddings = 1000
-        emb_dim = 16
-        tables = [
-            EmbeddingConfig(
-                num_embeddings=num_embeddings,
-                embedding_dim=emb_dim,
-                name=f"table_{i}",
-                feature_names=[f"feature_{i}"],
-            )
-            for i in range(3)
-        ]
-        model = KJTInputWrapper(
-            module_kjt_input=torch.nn.Sequential(
-                EmbeddingCollection(
-                    tables=tables,
-                    device=torch.device("cpu"),
-                )
-            )
-        )
-        non_sharded_model = quantize(
-            model,
-            inplace=False,
-            quant_state_dict_split_scale_bias=True,
-            weight_dtype=torch.qint8,
-        )
-        sharder = QuantEmbeddingCollectionSharder()
-        ddr_caps = [500, 100, 100]
-        topo_cpu = Topology(
-            world_size=3,
-            compute_device="cpu",
-            custom_topology_data=CustomTopologyData(
-                data={"ddr_cap": ddr_caps}, world_size=3
-            ),
-        )
-        topo_gpu = Topology(world_size=2, compute_device="cuda")
-        topo_groups = {
-            "cpu": topo_cpu,
-            "cuda": topo_gpu,
-        }
-        constraints = {
-            "table_0": ParameterConstraints(
-                device_group="cpu", sharding_types=[ShardingType.ROW_WISE.value]
-            ),
-            "table_1": ParameterConstraints(
-                device_group="cuda", sharding_types=[ShardingType.TABLE_WISE.value]
-            ),
-            "table_2": ParameterConstraints(
-                device_group="cuda", sharding_types=[ShardingType.TABLE_WISE.value]
-            ),
-        }
-        planner = HeteroEmbeddingShardingPlanner(
-            topology_groups=topo_groups, constraints=constraints
-        )
-        module_plan = planner.plan(
-            non_sharded_model,
-            # pyre-ignore
-            sharders=[sharder],
-        )
-
-        self.assertTrue(
-            # pyre-ignore
-            module_plan.plan["_module_kjt_input.0"]["table_0"]
-            .sharding_spec.shards[0]
-            .placement.device()
-            .type,
-            "cpu",
-        )
-        expected_row_sizes: List[int] = []
-        total_ddr_cap = sum(ddr_caps)
-        expected_row_sizes.append(int(num_embeddings * (ddr_caps[0] / total_ddr_cap)))
-        expected_row_sizes.append(int(num_embeddings * (ddr_caps[1] / total_ddr_cap)))
-        expected_row_sizes.append(num_embeddings - sum(expected_row_sizes))
-
-        for i in range(len(ddr_caps)):
-            self.assertEqual(
-                module_plan.plan["_module_kjt_input.0"]["table_0"]
-                .sharding_spec.shards[i]
-                .shard_sizes[0],
-                expected_row_sizes[i],
-            )
-
         self.assertTrue(
             module_plan.plan["_module_kjt_input.0"]["table_1"]
             .sharding_spec.shards[0]
