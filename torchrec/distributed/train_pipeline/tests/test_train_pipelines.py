ac4887f86c99bd49f8bbd82ca9018e3dfc67abd9
Yong Hoon Shin <yhshin@meta.com>
2024-06-10 19:09:26 -0700

    Fix FPEBC train pipeline test (#2090)
    Summary:
    Pull Request resolved: https://github.com/pytorch/torchrec/pull/2090
    
    3 issues that needed fixing:
    1) Move batch to GPU
    2) Set compute kernel to fused instead of dense to work w/ TW sharding
    3) Ensure that input batch idlist_features KJT has max length equal to the max lengths specified for feature processors (otherwise it would fail on `torch.gather()` in feature processor due to  shape mismatch between KJT input lengths and indices
    
    Reviewed By: henrylhtsang
    
    Differential Revision: D56950454

--- torchrec/distributed/train_pipeline/tests/test_train_pipelines.py
+++ torchrec/distributed/train_pipeline/tests/test_train_pipelines.py
@@ -178,20 +178,23 @@ class TrainPipelineSparseDistTest(TrainPipelineSparseDistTestBase):
             def forward(self, model_input) -> Tuple[torch.Tensor, torch.Tensor]:
                 return self.m(model_input.idlist_features)
 
+        max_feature_lengths = [10, 10, 12, 12]
         sparse_arch = DummyWrapper(
             create_module_and_freeze(
                 tables=self.tables,
                 device=self.device,
                 use_fp_collection=False,
+                max_feature_lengths=max_feature_lengths,
             )
         )
+        compute_kernel = EmbeddingComputeKernel.FUSED.value
         module_sharding_plan = construct_module_sharding_plan(
             sparse_arch.m._fp_ebc,
             per_param_sharding={
-                "table_0": table_wise(rank=0),
-                "table_1": table_wise(rank=0),
-                "table_2": table_wise(rank=0),
-                "table_3": table_wise(rank=0),
+                "table_0": table_wise(rank=0, compute_kernel=compute_kernel),
+                "table_1": table_wise(rank=0, compute_kernel=compute_kernel),
+                "table_2": table_wise(rank=0, compute_kernel=compute_kernel),
+                "table_3": table_wise(rank=0, compute_kernel=compute_kernel),
             },
             local_size=1,
             world_size=1,
@@ -219,7 +222,9 @@ class TrainPipelineSparseDistTest(TrainPipelineSparseDistTestBase):
             sharded_sparse_arch_pipeline.state_dict(),
         )
 
-        data = self._generate_data(num_batches=5, batch_size=1)
+        data = self._generate_data(
+            num_batches=5, batch_size=1, max_feature_lengths=max_feature_lengths
+        )
         dataloader = iter(data)
 
         optimizer_no_pipeline = optim.SGD(
@@ -236,6 +241,7 @@ class TrainPipelineSparseDistTest(TrainPipelineSparseDistTestBase):
         )
 
         for batch in data[:-2]:
+            batch = batch.to(self.device)
             optimizer_no_pipeline.zero_grad()
             loss, pred = sharded_sparse_arch_no_pipeline(batch)
             loss.backward()
