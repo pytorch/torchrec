67fb709452865f18e324c818767125e4a74aee17
Paul Zhang <paulzhan@umich.edu>
2024-06-17 13:24:53 +0000

    Update setup and version for release 0.8.0

--- setup.py
+++ setup.py
@@ -29,8 +29,8 @@ def _get_version():
     else:
         with open(os.path.join(ROOT_DIR, "version.txt"), "r") as f:
             version = f.readline().strip()
-        if sha is not None and "OFFICIAL_RELEASE" not in os.environ:
-            version += "+" + sha[:7]
+#        if sha is not None and "OFFICIAL_RELEASE" not in os.environ:
+#            version += "+" + sha[:7]
 
     if sha is None:
         sha = "Unknown"
