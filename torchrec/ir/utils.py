23fca828f9b9028ee8e67b897aa89811cd175750
pytorchbot <pytorchbot@pytorch.com>
2024-07-02 11:34:45 +0000

    2024-07-02 nightly release (a9d120f19052de1e9d3f38005e0cfc3a7f225c7a)

--- torchrec/ir/utils.py
+++ torchrec/ir/utils.py
@@ -9,6 +9,7 @@
 
 #!/usr/bin/env python3
 
+import logging
 from collections import defaultdict
 from typing import Dict, List, Optional, Tuple, Type, Union
 
@@ -24,6 +25,83 @@ from torchrec.sparse.jagged_tensor import KeyedJaggedTensor
 # TODO: Replace the default interface with the python dataclass interface
 DEFAULT_SERIALIZER_CLS = SerializerInterface
 DYNAMIC_DIMS: Dict[str, int] = defaultdict(int)
+logger: logging.Logger = logging.getLogger(__name__)
+
+
+@torch.library.custom_op("torchrec::ir_custom_op", mutates_args={})
+def ir_custom_op_impl(
+    tensors: List[Optional[torch.Tensor]], batch_size: int, dim: int
+) -> torch.Tensor:
+    device = None
+    for t in tensors:
+        if t is not None:
+            device = t.device
+            break
+    logger.info(f"torch.ops.torchrec.ir_custom_op -> ({batch_size}, {dim})")
+    return torch.empty(batch_size, dim, device=device)
+
+
+@torch.library.register_fake("torchrec::ir_custom_op")
+def ir_custom_op_fake(
+    tensors: List[Optional[torch.Tensor]], batch_size: int, dim: int
+) -> torch.Tensor:
+    logger.info(f"ir_custom_op_fake -> ({batch_size}, {dim})")
+    return torch.empty(batch_size, dim)
+
+
+def encapsulate_ir_modules(
+    module: nn.Module,
+    serializer: Type[SerializerInterface] = DEFAULT_SERIALIZER_CLS,
+    fqn: str = "",
+) -> Tuple[nn.Module, List[str]]:
+    """
+    Takes a module and encapsulate its embedding modules and serializes them to the module buffer.
+    Returns the modified module and a list of fqns that had the buffer added, which is needed for torch.export
+    The encapsulation is done by using meta_forward function provided by the serializer
+    to replace the module's original forward function.
+    """
+    preserve_fqns: List[str] = []  # fqns of the serialized modules
+    children: List[str] = []  # fqns of the children that need further serialization
+    # handle current module, and find the children which need further serialization
+    if type(module).__name__ in serializer.module_to_serializer_cls:
+        children = serializer.encapsulate_module(module)
+        preserve_fqns.append(fqn)
+    else:
+        # if the module is not of type serializer, then we check all its children
+        children = [child for child, _ in module.named_children()]
+
+    # handle child modules recursively
+    for child in children:
+        submodule = module.get_submodule(child)
+        child_fqn = f"{fqn}.{child}" if len(fqn) > 0 else child
+        _, fqns = encapsulate_ir_modules(submodule, serializer, child_fqn)
+        preserve_fqns.extend(fqns)
+    return module, preserve_fqns
+
+
+def decapsulate_ir_modules(
+    module: nn.Module,
+    serializer: Type[SerializerInterface] = DEFAULT_SERIALIZER_CLS,
+    device: Optional[torch.device] = None,
+) -> nn.Module:
+    """
+    Takes a module and decapsulate its embedding modules by retrieving the buffer.
+    Returns the module with restored embedding (sub) modules.
+    """
+    for child_fqn, child in module.named_children():
+        # perform deserialization on the children first, so that we can replace the child module with
+        # the deserialized module, and then replace it in the parent
+        child = decapsulate_ir_modules(
+            module=child, serializer=serializer, device=device
+        )
+        # replace the child module with deserialized one if applicable
+        setattr(module, child_fqn, child)
+
+    # only deserialize if the module has ir_metadata buffer, otherwise return as is
+    # we use "ir_metadata" as a convention to identify the deserializable module
+    if "ir_metadata" in dict(module.named_buffers()):
+        module = serializer.decapsulate_module(module, device)
+    return module
 
 
 def serialize_embedding_modules(
