6ee9a94e09c2df6acaf07d1ee00a7234e0c17243
Huanyu He <hhy@meta.com>
2024-07-09 09:51:17 -0700

    Reland [TorchRec][PT2] KJT custom op for 1d lengths input (#2183)
    Summary:
    Pull Request resolved: https://github.com/pytorch/torchrec/pull/2183
    
    # context
    * previous landed D59031938 was reverted due to torchscript push schedule is behind
    * adding `torch.jit.is_scripting()` to protect the exposure.
    
    Reviewed By: IvanKobzarev
    
    Differential Revision: D59081243

--- torchrec/sparse/jagged_tensor.py
+++ torchrec/sparse/jagged_tensor.py
@@ -1975,8 +1975,20 @@ class KeyedJaggedTensor(Pipelineable, metaclass=JaggedTensorMeta):
                 indices_tensor,
                 self.weights_or_none(),
             )
+        elif is_torchdynamo_compiling() and not torch.jit.is_scripting():
+            (
+                permuted_lengths,
+                permuted_values,
+                permuted_weights,
+            ) = torch.ops.fbgemm.permute_2D_sparse_data_input1D(
+                indices_tensor,
+                self.lengths(),
+                self.values(),
+                self.stride(),
+                self.weights_or_none(),
+                permuted_length_per_key_sum,
+            )
         else:
-
             (
                 permuted_lengths,
                 permuted_values,
@@ -2357,8 +2369,24 @@ class KeyedJaggedTensor(Pipelineable, metaclass=JaggedTensorMeta):
                         single_batch_per_rank = all(
                             s == stride for s in stride_per_rank
                         )
-
-                    if single_batch_per_rank:
+                    if (
+                        single_batch_per_rank
+                        and is_torchdynamo_compiling()
+                        and not torch.jit.is_scripting()
+                    ):
+                        (
+                            lengths,
+                            values,
+                            weights,
+                        ) = torch.ops.fbgemm.permute_2D_sparse_data_input1D(
+                            torch.jit._unwrap_optional(recat),
+                            lengths,
+                            values,
+                            stride,
+                            weights,
+                            values.numel(),
+                        )
+                    elif single_batch_per_rank:
                         (
                             lengths,
                             values,
