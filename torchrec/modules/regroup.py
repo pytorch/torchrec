bb140accf68b204287aec4354d6e88f1bdd118fe
Paul Zhang <paulzhan@meta.com>
2024-07-29 12:17:09 -0700

    Fix device issue with regroup module
    Summary:
    When using the new RegroupAsDict module with cached tensors, ex. self._permutes, they aren't properly moved to the right devices during the model processing pipeline. This is due to the fact that the module is not a leaf module and does not have a `set_device` method.
    
    This diff creates a submodule `PermutePooledEmbeddings` that just handles the fbgemm logic and holds the cached tensors (for moving to the right device), along with adding this module to the leaf modules for AIMP.
    
    Differential Revision: D60062406

--- torchrec/modules/regroup.py
+++ torchrec/modules/regroup.py
@@ -12,6 +12,7 @@
 from typing import Dict, List, Optional, Tuple, Union
 
 import torch
+from pyre_extensions import none_throws
 from torchrec.sparse.jagged_tensor import _desugar_keyed_tensors, KeyedTensor
 
 
@@ -40,6 +41,56 @@ def _build_dict(
         return {key: st for key, st in zip(keys, torch.split(values, splits, dim=dim))}
     else:
         return {key: tensor for key, tensor in zip(keys, values)}
+
+
+class PermuteMultiEmbedding(torch.nn.Module):
+    """
+    Module to handle cached tensors and running FBGEMM
+    op for KT. This separate module allows fx tracing through
+    all the logic in KTRegroupAsDict while keeping what's necessary
+    for exposing set_device and allowing tensors to be moved to
+    the appropriate device during model processing.
+
+    Args:
+        groups (List[List[str]]): Groups from KTRegroupAsDict
+
+    """
+
+    def __init__(self, groups: List[List[str]]) -> None:
+        super().__init__()
+        self._groups = groups
+        self._permute_tensor: Optional[torch.Tensor] = None
+        self._in_shapes: Optional[torch.Tensor] = None
+        self._out_shapes: Optional[torch.Tensor] = None
+        self._out_lengths: Optional[List[int]] = None
+
+    def init_tensors(
+        self,
+        permute: torch.Tensor,
+        in_shapes: torch.Tensor,
+        out_shapes: torch.Tensor,
+        out_lengths: List[int],
+    ) -> None:
+        # no need to pin_memory() or to(..., non_blocking=True) since occurs only once
+        self._permute_tensor = permute
+        self._in_shapes = in_shapes
+        self._out_shapes = out_shapes
+        self._out_lengths = out_lengths
+
+    @torch.jit.export
+    def set_device(self, device: str) -> None:
+        self._permute_tensor = none_throws(self._permute_tensor).to(device)
+        self._in_shapes = none_throws(self._in_shapes).to(device)
+        self._out_shapes = none_throws(self._out_shapes).to(device)
+
+    def forward(self, values: List[torch.Tensor]) -> List[torch.Tensor]:
+        return torch.ops.fbgemm.permute_multi_embedding(
+            values,
+            self._permute_tensor,
+            self._in_shapes,
+            self._out_shapes,
+            self._out_lengths,
+        )
 
 
 class KTRegroupAsDict(torch.nn.Module):
@@ -74,26 +125,30 @@ class KTRegroupAsDict(torch.nn.Module):
         self._is_inited = False
 
         # cached values populated on first forward call
-        self.device: Optional[torch.device] = None
+        self.device: Optional[str] = None
         self._concat_dim: int = 1
         self._use_fbgemm_regroup: bool = False
         self._splits: List[int] = []
         self._idx_key_pairs: List[Tuple[int, str]] = []
-        self._permutes: Optional[torch.Tensor] = None
-        self._in_shapes: Optional[torch.Tensor] = None
-        self._out_shapes: Optional[torch.Tensor] = None
-        self._out_lengths: Optional[List[int]] = None
+        self._permute_pooled_embs_impl = PermuteMultiEmbedding(groups)
 
     def _init_fbgemm_regroup(self, kts: List[KeyedTensor]) -> None:
         self._use_fbgemm_regroup = True
         keys, lengths, values = _desugar_keyed_tensors(kts)
-        self._permutes, self._in_shapes, self._out_shapes, self._out_lengths = (
+        permutes, in_shapes, out_shapes, out_lengths = (
             torch.ops.fbgemm.kt_regroup_arguments(
                 values[0],
                 keys,
                 lengths,
                 self._groups,
             )
+        )
+        # no need to pin_memory() or to(..., non_blocking=True) since occurs only once
+        self._permute_pooled_embs_impl.init_tensors(
+            permutes,
+            in_shapes,
+            out_shapes,
+            out_lengths,
         )
 
     def _init_regroup(self, kts: List[KeyedTensor]) -> None:
@@ -129,7 +184,7 @@ class KTRegroupAsDict(torch.nn.Module):
             assert all(
                 kt.device() == keyed_tensors[0].device() for kt in keyed_tensors
             ), "All inputs should be on the same device."
-            self.device = keyed_tensors[0].device()
+            self.device = keyed_tensors[0].device().type
             assert all(
                 kt.key_dim() == keyed_tensors[0].key_dim() for kt in keyed_tensors
             ), "All inputs should have the same key_dim"
@@ -143,14 +198,7 @@ class KTRegroupAsDict(torch.nn.Module):
 
         if self._use_fbgemm_regroup:
             values = _get_kts_values(keyed_tensors)
-            permuted_values = torch.ops.fbgemm.permute_multi_embedding(
-                values,
-                self._permutes,
-                self._in_shapes,
-                self._out_shapes,
-                self._out_lengths,
-            )
-            # return {key: tensor for key, tensor in zip(self._keys, permuted_values)}
+            permuted_values = self._permute_pooled_embs_impl(values)
         else:
             permuted_values = _permuted_values(
                 keyed_tensors, self._idx_key_pairs, self._dim
