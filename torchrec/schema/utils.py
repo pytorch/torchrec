4094f0e2b3458098d5bcc571a5a18f89de8568e7
Felicity Liao <11263993+aporialiao@users.noreply.github.com>
2024-09-10 15:54:25 -0700

    BC Unit Test Framework for Stable APIs (#2372)
    Summary:
    Pull Request resolved: https://github.com/pytorch/torchrec/pull/2372
    
    Introduce unit test framework for assessing BC compatibility for stable APIs in TorchRec
    
    Differential Revision: D62389250

---
+++ torchrec/schema/utils.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+
+import inspect
+
+
+def is_signature_compatible(
+    expected_signature: inspect.Signature,
+    current_signature: inspect.Signature,
+) -> bool:
+    """Check if two signatures are compatible.
+
+    Args:
+        sig1: The first signature.
+        sig2: The second signature.
+        ignore_types: Whether to ignore types when checking compatibility.
+        ignore_names: Whether to ignore names when checking compatibility.
+
+    Returns:
+        True if the signatures are compatible, False otherwise.
+
+    """
+
+    # If current signature has less parameters than expected signature
+    # BC is automatically broken, no need to check further
+    if len(expected_signature.parameters) > len(current_signature.parameters):
+        return False
+
+    # Check order of positional arguments
+    expected_args = list(expected_signature.parameters.values())
+    current_args = list(current_signature.parameters.values())
+
+    expected_keyword_only_args = set()
+    current_keyword_only_args = set()
+
+    for i in range(len(expected_args)):
+        expected_arg = expected_args[i]
+        current_arg = current_args[i]
+
+        # If the kinds of arguments are different, BC is broken
+        # unless current arg is a keyword argument
+        if expected_arg.kind != current_arg.kind:
+            if expected_arg.kind == expected_arg.VAR_KEYWORD:
+                # Any arg can be inserted before **kwargs and still maintain BC
+                continue
+            else:
+                return False
+
+        # Potential positional arguments need to have the same name
+        # keyword only arguments can be mixed up
+        if expected_arg.kind == expected_arg.POSITIONAL_OR_KEYWORD:
+            if expected_arg.name != current_arg.name:
+                return False
+
+            # Positional arguments need to have the same type annotation
+            # TODO: Account for Union Types?
+            if expected_arg.annotation != current_arg.annotation:
+                return False
+
+            # Positional arguments need to have the same default value
+            if expected_arg.default != current_arg.default:
+                return False
+        elif expected_arg.kind == expected_arg.KEYWORD_ONLY:
+            # Store the names of all keyword only arguments
+            # to check if all expected keyword only arguments
+            # are present in current signature
+            expected_keyword_only_args.add(expected_arg.name)
+            current_keyword_only_args.add(current_arg.name)
+
+    # All kwargs in expected signature must be present in current signature
+    for kwarg in expected_keyword_only_args:
+        if kwarg not in current_keyword_only_args:
+            return False
+
+    # TODO: Account for Union Types?
+    if current_signature.return_annotation != expected_signature.return_annotation:
+        return False
+    return True
