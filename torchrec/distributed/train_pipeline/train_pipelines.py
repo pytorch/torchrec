87dd0929264068ce2890edbe37ead9eebe70be2e
Shabab Ayub <shababayub@meta.com>
2024-10-23 09:22:24 -0700

    another torch deploy protection :(
    Summary: this import doesnt work in torch dceploy for whatever reason (we're getting rid of it all in 2 months so just bare with this for now)
    
    Differential Revision: D64830919

--- torchrec/distributed/train_pipeline/train_pipelines.py
+++ torchrec/distributed/train_pipeline/train_pipelines.py
@@ -74,7 +74,8 @@ try:
 except ImportError:
     logger.warning("torchrec_use_sync_collectives is not available")
 
-torch.ops.import_module("fbgemm_gpu.sparse_ops")
+if not torch._running_with_deploy():
+    torch.ops.import_module("fbgemm_gpu.sparse_ops")
 
 
 class ModelDetachedException(Exception):
