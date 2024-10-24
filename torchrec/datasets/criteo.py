Fix CPU unit tests CI int overflow in preproc criteo (#2522)
Summary: Pull Request resolved: https://github.com/pytorch/torchrec/pull/2522

Differential Revision: D64919343

torchrec/datasets/criteo.py
@@ -252,7 +252,7 @@ class BinaryCriteoUtils:
         # using int64. Numpy will automatically handle dense values >= 2 ** 31.
         dense_np = np.array(dense, dtype=np.int32)
         del dense
-        sparse_np = np.array(sparse, dtype=np.int32)
+        sparse_np = np.array(sparse, dtype=np.int64)
         del sparse
         labels_np = np.array(labels, dtype=np.int32)
         del labels

torchrec/datasets/tests/test_criteo.py
@@ -144,7 +144,7 @@ class TestBinaryCriteoUtils(CriteoTest):
             self.assertEqual(dense.shape, (num_rows, INT_FEATURE_COUNT))
             self.assertEqual(dense.dtype, np.float32)
             self.assertEqual(sparse.shape, (num_rows, CAT_FEATURE_COUNT))
-            self.assertEqual(sparse.dtype, np.int32)
+            self.assertEqual(sparse.dtype, np.int64)
             self.assertEqual(labels.shape, (num_rows, 1))
             self.assertEqual(labels.dtype, np.int32)
 