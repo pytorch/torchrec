39133cc30e863077e1ec7232d539bff3d876fbf8
Huanyu He <hhy@meta.com>
2024-11-25 12:13:29 -0800

    adding tensordict into targets to avoid package issue (#2593)
    Summary:
    
    to avoid test failures like the following: https://www.internalfb.com/diff/D65103519?dst_version_fbid=450279051096758
    {F1967377517}
    
    Differential Revision: D66465376

--- torchrec/distributed/embedding.py
+++ torchrec/distributed/embedding.py
@@ -96,6 +96,14 @@ try:
 except OSError:
     pass
 
+try:
+    from tensordict import TensorDict
+except ImportError:
+
+    class TensorDict:
+        pass
+
+
 logger: logging.Logger = logging.getLogger(__name__)
 
 
