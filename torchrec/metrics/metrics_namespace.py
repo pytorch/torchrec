8ca8b65c86525b7fe871c1561bf73123b1408851
Chenyu Zhao <chenyuzhao@meta.com>
2024-12-05 01:47:08 -0800

    add GAUC(Group-level AUC) metric. (#2614)
    Summary:
    Pull Request resolved: https://github.com/pytorch/torchrec/pull/2614
    
    Add GAUC metric to align offline/online metrics.
    
    Reviewed By: yunjiangster
    
    Differential Revision: D66349373
    
    fbshipit-source-id: f7f831f237e61cc9ad0b8ddd6e596868ab41ae93

--- torchrec/metrics/metrics_namespace.py
+++ torchrec/metrics/metrics_namespace.py
@@ -52,6 +52,7 @@ class MetricName(MetricNameBase):
     MAE = "mae"
     RMSE = "rmse"
     AUC = "auc"
+    GAUC = "gauc"
     AUPRC = "auprc"
     RAUC = "rauc"
     GROUPED_AUC = "grouped_auc"
@@ -68,6 +69,7 @@ class MetricName(MetricNameBase):
     SCALAR = "scalar"
     OUTPUT = "output"
 
+    GAUC_NUM_SAMPLES = "gauc_num_samples"
     TOTAL_POSITIVE_EXAMPLES = "total_positive_examples"
     TOTAL_NEGATIVE_EXAMPLES = "total_negative_examples"
     PRECISION = "precision"
@@ -96,6 +98,7 @@ class MetricNamespace(MetricNamespaceBase):
     CALIBRATION = "calibration"
     MSE = "mse"
     AUC = "auc"
+    GAUC = "gauc"
     AUPRC = "auprc"
     RAUC = "rauc"
     MAE = "mae"
