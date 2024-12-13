3928a1b0be696a62abe6ddda43a4706449af4741
Yong Hoon Shin <yhshin@meta.com>
2024-12-13 03:25:01 -0800

    Fix invalid record_stream arg for Request awaitable (#2634)
    Summary:
    Pull Request resolved: https://github.com/pytorch/torchrec/pull/2634
    
    https://github.com/pytorch/torchrec/pull/2598 causes failures when running on cpu:
    
    ```
    RuntimeError: unknown parameter type
    ```
    
    This is because `record_stream` doesn't accept CPU streams. This diff forward fixes the issue.
    
    Differential Revision: D67171994
    
    fbshipit-source-id: a9ffddfcd978d343a58fb039a4fcd89c336ceee6

--- torchrec/distributed/tests/test_sharding_plan.py
+++ torchrec/distributed/tests/test_sharding_plan.py
@@ -119,10 +119,6 @@ def _test_sharding(
 
 @skip_if_asan_class
 class ConstructParameterShardingAndShardTest(MultiProcessTestBase):
-    @unittest.skipIf(
-        torch.cuda.device_count() <= 1,
-        "Not enough GPUs, this test requires at least two GPUs",
-    )
     # pyre-fixme[56]
     @given(
         per_param_sharding=st.sampled_from(
