7a1a6907cd2e99256dbe9ff9b8b3bda32ba9d5d9
Yong Hoon Shin <yhshin@meta.com>
2025-01-06 15:36:27 -0800

    Update gcc version for FBGEMM install in CI (#2654)
    Summary:
    
    TorchRec CI currently is failing with issues on incompatible GLIBCXX version. The cause is that FBGEMM now requires g++ 11.1+ for building binaries that reference GLIBCXX_3.4.29 (as of https://github.com/pytorch/pytorch/pull/141035)
    
    As recommended in https://github.com/pytorch/FBGEMM/blob/main/.github/scripts/utils_build.bash and https://github.com/pytorch/FBGEMM/issues/3423, install GCC using conda to control glibcxx version being used.
    
    Differential Revision: D67607624

--- .github/scripts/install_fbgemm.sh
+++ .github/scripts/install_fbgemm.sh
@@ -12,7 +12,7 @@ echo "CHANNEL"
 echo "$CHANNEL"
 
 if [ "$CHANNEL" = "nightly" ]; then
-    ${CONDA_RUN} pip install fbgemm-gpu --index-url https://download.pytorch.org/whl/nightly/"$CU_VERSION"
+    ${CONDA_RUN} pip install --pre fbgemm-gpu --index-url https://download.pytorch.org/whl/nightly/"$CU_VERSION"
 elif [ "$CHANNEL" = "test" ]; then
-    ${CONDA_RUN} pip install fbgemm-gpu --index-url https://download.pytorch.org/whl/test/"$CU_VERSION"
+    ${CONDA_RUN} pip install --pre fbgemm-gpu --index-url https://download.pytorch.org/whl/test/"$CU_VERSION"
 fi
