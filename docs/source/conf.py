0329ba9804aeceece0287655f4932bb949f2619b
Svetlana Karslioglu <svekars@meta.com>
2025-01-16 15:45:16 -0800

    Add Google Analytics code

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -87,6 +87,7 @@ html_theme_options = {
     "logo_only": True,
     "collapse_navigation": False,
     "includehidden": True,
+    'analytics_id': 'GTM-T8XT4PS',
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
@@ -95,3 +96,4 @@ html_theme_options = {
 html_static_path = ["_static"]
 
 html_css_files = ["css/custom.css"]
+
