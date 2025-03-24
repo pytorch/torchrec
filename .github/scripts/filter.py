44578d076b24cb0b4429143cff96ee69170dc2d0
Felicity Liao <felicity@meta.com>
2025-03-24 09:42:55 -0700

    Enable Cuda 12.6 since FBGEMM offers it now (#2845)
    Summary:
    Pull Request resolved: https://github.com/pytorch/torchrec/pull/2845
    
    I believe this should resolve cu126 errors on nightly binaries as well.
    
    Before we filtered out CUDA version 12.6 because FBGEMM (torchrec dependency) did not support it yet, but now there is support - seen from https://download.pytorch.org/whl/nightly/cu126 with `fbgemm_gpu` available - so we should remove our filter.
    
    Our filter was causing issue with CU126 nightly binaries because there is no torchrec version supported.
     {F1976345189}
    
    Reviewed By: aliafzal
    
    Differential Revision: D71707060
    
    fbshipit-source-id: 70877d6d46e749203d3f11f72973f38fa8b32f16

--- .github/scripts/filter.py
+++ .github/scripts/filter.py
@@ -11,9 +11,9 @@ import os
 
 def main():
     """
-    Since FBGEMM doesn't publish CUDA 12.6 binaries yet, torchrec will not work with
-    CUDA 12.6. As a result, we filter out CUDA 12.6 from the build matrix that
-    determines with nightly builds are run.
+    For filtering out certain cuda versions in the build matrix that
+    determines with nightly builds are run. This ensures TorchRec is
+    always consistent in version compatibility with FBGEMM.
     """
 
     full_matrix_string = os.environ["MAT"]
@@ -22,8 +22,7 @@ def main():
     new_matrix_entries = []
 
     for entry in full_matrix["include"]:
-        if entry["gpu_arch_version"] != "12.6":
-            new_matrix_entries.append(entry)
+        new_matrix_entries.append(entry)
 
     new_matrix = {"include": new_matrix_entries}
     print(json.dumps(new_matrix))
