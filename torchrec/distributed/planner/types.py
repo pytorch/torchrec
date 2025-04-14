9367da7384ab11dc1554da536fee013aee4e91a4
pytorchbot <pytorchbot@pytorch.com>
2025-04-14 11:34:55 +0000

    2025-04-14 nightly release (05aea060bc92f07f4aba057f4631aa2d2029b2ad)

--- torchrec/distributed/planner/types.py
+++ torchrec/distributed/planner/types.py
@@ -810,3 +810,12 @@ class Stats(abc.ABC):
         See class description
         """
         ...
+
+
+@dataclass
+class CriticalPathEstimate:
+    comms_estimate: float
+    comp_estimate: float
+
+    def total(self) -> float:
+        return self.comms_estimate + self.comp_estimate
